5443d7bc9ee662901be35a927e675fec4e72d052
Marko Grdinić <mrakgr@gmail.com>
2017-12-17 19:38:45 +0100

    Well, I will deal with it tomorrow. I am way too tired for this currently. I should be prepared to spend a while drilling into this. I will put the tutorials on hold for the time being.

--- Temporary/output.fs
+++ Temporary/output.fs
@@ -6,19 +6,47 @@ extern "C" {
 }
 """
 
-let rec method_0 (): int64 =
-    0L
-and method_1((var_0: (unit -> int64)), (var_1: int64)): int64 =
-    let (var_2: bool) = (var_1 < 10L)
-    if var_2 then
-        let (var_3: int64) = (var_1 + 1L)
-        let (var_4: (unit -> int64)) = method_2((var_0: (unit -> int64)))
-        method_1((var_4: (unit -> int64)), (var_3: int64))
-    else
-        var_0()
-and method_2 ((var_0: (unit -> int64))) (): int64 =
-    let (var_1: int64) = var_0()
-    (var_1 + 1L)
-let (var_0: (unit -> int64)) = method_0
-let (var_1: int64) = 0L
-method_1((var_0: (unit -> int64)), (var_1: int64))
+type Rec0 =
+    | Rec0Case0 of Tuple2
+    | Rec0Case1 of Tuple3
+    | Rec0Case2 of Tuple1
+and Tuple1 =
+    struct
+    val mem_0: int64
+    new(arg_mem_0) = {mem_0 = arg_mem_0}
+    end
+and Tuple2 =
+    struct
+    val mem_0: Rec0
+    val mem_1: Rec0
+    new(arg_mem_0, arg_mem_1) = {mem_0 = arg_mem_0; mem_1 = arg_mem_1}
+    end
+and Tuple3 =
+    struct
+    val mem_0: Rec0
+    val mem_1: Rec0
+    new(arg_mem_0, arg_mem_1) = {mem_0 = arg_mem_0; mem_1 = arg_mem_1}
+    end
+let rec method_1((var_0: Rec0), (var_1: Rec0)): int64 =
+    let (var_2: int64) = method_2((var_0: Rec0))
+    let (var_3: int64) = method_2((var_1: Rec0))
+    (var_2 * var_3)
+and method_2((var_0: Rec0)): int64 =
+    match var_0 with
+    | Rec0Case0(var_1) ->
+        let (var_4: Rec0) = var_1.mem_0
+        let (var_5: Rec0) = var_1.mem_1
+        let (var_6: int64) = method_2((var_4: Rec0))
+        let (var_7: int64) = method_2((var_5: Rec0))
+        (var_6 + var_7)
+    | Rec0Case1(var_2) ->
+        let (var_9: Rec0) = var_2.mem_0
+        let (var_10: Rec0) = var_2.mem_1
+        let (var_11: int64) = method_2((var_9: Rec0))
+        let (var_12: int64) = method_2((var_10: Rec0))
+        (var_11 * var_12)
+    | Rec0Case2(var_3) ->
+        var_3.mem_0
+let (var_0: Rec0) = (Rec0Case0(Tuple2((Rec0Case2(Tuple1(1L))), (Rec0Case2(Tuple1(2L))))))
+let (var_1: Rec0) = (Rec0Case0(Tuple2((Rec0Case2(Tuple1(3L))), (Rec0Case2(Tuple1(4L))))))
+method_1((var_0: Rec0), (var_1: Rec0))
--- Testing/run.fs
+++ Testing/run.fs
@@ -12,20 +12,26 @@ let cfg: Spiral.Types.CompilerSettings = {
     cuda_includes = []
     }
 
-rewrite_test_cache cfg None //(Some(0,40))
+//rewrite_test_cache cfg None //(Some(0,40))
 
 let example1 = 
     "example1",[],"",
     """
-inl rec loop f i =
-    inl f, i = term_cast f (), dyn i
-    inl body _ = if i < 10 then loop (inl _ -> f() + 1) (i + 1) else f()
-    join (body() : int64)
+if dyn true then
+    inl a,b = dyn (1,2)
+    inl _ -> a + b
+else
+    inl a,b = dyn (3,4)
+    inl _ -> a + b
+//inl rec loop f i =
+//    inl f, i = term_cast f (), dyn i
+//    inl body _ = if i < 10 then loop (inl _ -> f() + 1) (i + 1) else f()
+//    join (body() : int64)
 
-loop (inl _ -> 0) 0
+//loop (inl _ -> 0) 0
     """
 
 //output_test_to_temp {cfg with cuda_includes=["cub/cub.cuh"]} @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" learning
-output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" example1
+output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" test14
 |> printfn "%s"
 |> ignore
