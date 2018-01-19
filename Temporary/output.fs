c57f3dbb75a1c4382517dbe93bcac63e5b4c83ab
Marko Grdinić <mrakgr@gmail.com>
2018-01-19 11:23:30 +0100

    Trying to find the synchronization bug is a dead end. I can't get it to replicate. Forget it, there is no point in bashing my head against this.
    I should move from here.
    
    With this out of the way, what I should do is avoid using types lower than 4 bytes in the language.
    
    I'll go to what I was doing previously which was summing up floats in acucracy.
    
    Edit: Actually, I will take kunzmi's advice and restrict chars and and bools from traversing language join points.

--- Temporary/output.fs
+++ Temporary/output.fs
@@ -1,12 +1,28 @@
 open ManagedCuda
 open ManagedCuda.BasicTypes
+open System
+open System.Runtime.InteropServices
+Marshal.SizeOf(typeof<int8>)
+
 
 let ctx = new CudaContext()
 
-let size = 10240
-let x = new CudaDeviceVariable<bool>(SizeT size)
-x.SizeInBytes |> printfn "%A" // is size * 4
+let rng = Random()
+let size = 30
 
-for i=1 to 10 do
-    let y = Array.zeroCreate<bool>(size*3) // It does work with x4 though, but not with x1, x2 and x3 as shown here.
+let orig = Array.init size (fun _ -> rng.NextDouble() |> float32)
+
+let x = new CudaDeviceVariable<_>(SizeT size)
+x.CopyToDevice(orig)
+
+let y = Array.zeroCreate<float32>(size)
+x.CopyToHost(y)
+
+
+
+for i=1 to 1024 do
+    let y = Array.zeroCreate<float32>(size)
     x.CopyToHost(y)
+    if orig <> y then
+        Array.zip orig y |> printfn "%A"
+        failwith "Transfer failed."
