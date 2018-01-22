62bf63d4cbc431813f99ca9c5054d2350c6e2c16
Marko Grdinić <mrakgr@gmail.com>
2018-01-22 11:14:35 +0100

    " let path = @"C:\ML Datasets\TinyShakespeare\tiny_shakespeare.txt"
    let text = File.ReadAllText path
    
    let symbols = text.ToCharArray() |> Array.groupBy id |> Array.map (fun (a,b) -> a,b.Length)
    
    So it has 65 different symbols.
    I guess I'll set the one-hot vector size to 96 then.
    While loading the chars...
    Well, 96*4 = 400Mb. Having the preprocessing done would be doable in this case but...
    val it : int [] =
      [|70; 105; 114; 115; 116; 32; 67; 122; 101; 110; 58; 10; 66; 102; 111; 119;
        112; 99; 100; 97; 121; 117; 104; 44; 109; 107; 46; 65; 108; 83; 89; 118;
        63; 82; 77; 87; 39; 76; 73; 78; 103; 59; 98; 33; 79; 106; 86; 45; 84; 72;
        69; 85; 68; 80; 113; 120; 74; 71; 75; 81; 38; 90; 88; 51; 36|]
    
    Maybe I should make the size of the one hot vector 128 and save myself the effort?
    
    Yeah, there is no need to optimize it for the exact alphabet.
    
    I should leave that for the later step.
    
    Let me check out how Karpathy does it.
    "

--- Learning/Script1.fsx
+++ Learning/Script1.fsx
@@ -1,1 +1,11 @@
-﻿
+﻿open System.IO
+
+let path = @"C:\ML Datasets\TinyShakespeare\tiny_shakespeare.txt"
+
+let text = File.ReadAllText path
+
+let symbols = text.ToCharArray() |> Array.groupBy id |> Array.map (fun (a,b) -> a,b.Length)
+
+symbols 
+|> Array.map (fst >> int)
+|> Array.max
