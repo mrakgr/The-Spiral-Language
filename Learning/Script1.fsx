6d3830b3b001a5fe36e0b7b65d6122ae4a2d1845
Marko Grdinić <mrakgr@gmail.com>
2018-01-30 14:22:51 +0100

    "2pm. Ok, I understand now how to do the softmax backwards. Let me take a look at the broadcast kernel. I think I might need to reuse it for that.
    2:05pm. What I need to reuse in order to get the thing I am seeing is the `s_i * e_i` vector.
    
    That would give me following equation.
    
    ```
    e_i * h_i * (1 - h_i) - h_i * (sum - e_i * h_i)
    ```
    
    This is the true error. `d1_broadcast` was pretty much made for both the forward and the backwards pass it seems. Also since the gradients are only dependent on the output, I think I should in fact do the numerically stable version of this."
    
    I'll have to make an extra kernel for that though.
    
    Or maybe I will just hack the broadcast one so as to put an optional reduce into it?
    
    ...No actually, what I need to do is in fact put in multiple broadcasts.
    
    ...Yeah, that is what I will do. That will get me the maximum genericity and reuse.
    
    Ok, let me take a break here and I will resume programming after that. To think that before today I did not understand that the error vector needs to multiply the jacobian.
    
    Well, I am definitely learning new things as I go along. Now that I have Spiral, it is like the dam has burst and I can finally move forward.

--- Learning/Script1.fsx
+++ Learning/Script1.fsx
@@ -10,3 +10,11 @@ symbols
 |> Array.map (fst >> int)
 |> Array.max
 
+let s = [|0.2;0.3;0.5|]
+
+let f i j =
+    if i = j then s.[i]*(1.0-s.[j])
+    else -s.[i]*s.[j]
+
+let g i = f 0 i + f 1 i + f 2 i
+g 0
