029414a452e14957191ca44919f1492e4d7b8c99
Marko Grdinić <mrakgr@gmail.com>
2018-02-09 15:25:29 +0100

    Right now I am thinking what comes next.
    I have no choice. I will put my plans on the side and just get to work on rewriting the library using the new allocator.

--- Learning/Module.fs
+++ Learning/Module.fs
@@ -7,7 +7,7 @@ let allocator =
     (
     "Allocator",[option;extern_;console],"The stack based GPU memory allocator module.",
     """
-inl {Cuda size} ret ->
+inl {Cuda} ->
     open Cuda
     open Extern
     inl smartptr_create (ptr: uint64) =
@@ -21,63 +21,60 @@ inl {Cuda size} ret ->
             x
         |> stack
 
-    /////// n is the number of args the create function has.
-    //inl safe_alloc n create =
-    //    if lit_is n = false then error_type "n need to be static."
-    //    inl rec loop vars = function
-    //        | 0 ret ->
-    //            inl tns = Tuple.foldr (inl x create -> create x) vars create
-    //            inl r = ret tns
-    //            tns.update_body (inl {ar} -> ar.ptr.Dispose) |> ignore
-    //            r
-    //        | n x -> loop (x :: vars) (n-1)
-    //    function
-    //    | .unsafe -> create
-    //    | x -> loop () n x
+    met rec create_region x =
+        inl mult = 256u64
+        inl round_up_to_multiple size = size - size % mult + mult
+        inl pool =
+            inl f size = to uint64 size |> round_up_to_multiple
+            match x with
+            | {region size=!f size} -> { size ptr = region size }
+            | !f size -> { size ptr = FS.Method context .AllocateMemory (SizeT size) CUdeviceptr_type |> to_uint |> smartptr_create }
 
-    //inl to_float x = FS.UnOp .float x float64
-    //inl to_int x = FS.UnOp .int64 x int64
-    
-    //inl pool = 
-    //    inl size = 
-    //        match size with
-    //        | _ : float64 -> 
-    //            FS.Method context .GetFreeDeviceMemorySize() SizeT_type
-    //            |> to_int |> to_float |> (*) size |> to_uint
-    //        | _ : int64 -> to uint64 size
-    //    {size ptr=FS.Method context .AllocateMemory (SizeT size) CUdeviceptr_type |> to_uint |> smartptr_create}
+        inl pool_type = type pool
+        inl stack_type = fs [text: "System.Collections.Generic.Stack"; types: pool_type]
+        inl stack = FS.Constructor stack_type ()
 
-    //inl pool_type = type pool
-    //inl stack_type = fs [text: "System.Collections.Generic.Stack"; types: pool_type]
-    //inl stack = FS.Constructor stack_type ()
-
-    //inl allocate =
-    //    inl smartptr_ty = type pool.ptr
-    //    inl f {ptr size} = ptr(), size
-    //    inl pool_ptr, pool_size = f pool
-    //    met rec remove_disposed_and_return_the_first_live ret =
-    //        if FS.Method stack .get_Count() int32 > 0i32 then 
-    //            inl {ptr size} = FS.Method stack .Peek() pool_type
-    //            match ptr.Try with
-    //            | 0u64 -> FS.Method stack .Pop() pool_type |> ignore; remove_disposed_and_return_the_first_live ret 
-    //            | ptr -> join (ret (ptr, size))
+        inl allocate =
+            inl smartptr_ty = type pool.ptr
+            inl f {ptr size} = ptr(), size
+            inl pool_ptr, pool_size = f pool
+            met rec remove_disposed_and_return_the_first_live ret =
+                if FS.Method stack .get_Count() int32 > 0i32 then 
+                    inl {ptr size} = FS.Method stack .Peek() pool_type
+                    match ptr.Try with
+                    | 0u64 -> FS.Method stack .Pop() pool_type |> ignore; remove_disposed_and_return_the_first_live ret 
+                    | ptr -> join (ret (ptr, size))
                 
-    //        else join (ret (pool_ptr, 0u64))
-    //        : smartptr_ty
-    //    inl round_up_to_multiple mult size = size - size % mult + mult
-    //    inl s = to uint64 >> round_up_to_multiple 256u64 >> dyn
-    //    inl (!s size) ->
-    //        inb top_ptr, top_size = remove_disposed_and_return_the_first_live
-    //        inl top_used = top_ptr + top_size
-    //        inl pool_used = pool_ptr + pool_size
-    //        assert (size <= pool_used - top_used) "Cache size has been exceeded in the allocator."
-    //        inl ptr = top_used |> smartptr_create
-    //        FS.Method stack .Push {size ptr} unit
-    //        ptr
+                else join (ret (pool_ptr, 0u64))
+                : smartptr_ty
+            
+            inl s = to uint64 >> round_up_to_multiple >> dyn
+            inl (!s size) ->
+                inb top_ptr, top_size = remove_disposed_and_return_the_first_live
+                inl top_used = top_ptr + top_size
+                inl pool_used = pool_ptr + pool_size
+                assert (size <= pool_used - top_used) "Cache size has been exceeded in the allocator."
+                inl ptr = top_used |> smartptr_create
+                FS.Method stack .Push {size ptr} unit
+                ptr
 
-    //ret {allocate safe_alloc}
+        inl region = function
+            | .split size -> create_region {allocate size}
+            | x -> allocate x
 
-    //inl ptr = pool.ptr
-    //FS.Method context .FreeMemory (ptr() |> CUdeviceptr) unit
-    //ptr.Dispose
+        met rec remove_all () = 
+            if FS.Method stack .get_Count() int32 > 0i32 then 
+                FS.Method stack .Pop() pool_type .Dispose
+                remove_all()
+            : ()
+
+        inl ret ->
+            ret region
+
+            remove_all()
+            inl ptr = pool.ptr
+            FS.Method context .FreeMemory (ptr() |> CUdeviceptr) unit
+            ptr.Dispose
+
+    {create_region}
     """) |> module_
