b9622350f79e17c5b6d39646e9bbc4923499c1be
Marko Grdinić <mrakgr@gmail.com>
2018-02-12 18:59:24 +0100

    The first test works now and I am done for the day. Here is the section based allocator. It will serve as the basis for the region based one underneath which I will start work on tomorrow.
    I should have done this shit from the outset.
    
    Oh well, live and learn. This is the price of education.
    
    I am feeling under pressure. It was always like this, but I am absolutely determined not to miss 2018, like I did 2016 and 2017. I can't afford many more years where I get sidetracked. Without a doubt, I must complete the ML library and make the first batch of agents to represent my power.
    
    Right now I simply too vulnerable to accidents. If I encounter even a single situation that would require me to make immediate money, my Singularity run will be ruined due me having to cease development on Spiral.
    
    The Spiral language that I have now is the firm foundation for my programming that I needed. It will be enough even without being perfected. Its basic form will be enough and I will unlocking the full power of partial evaluation to the future Inspired me.
    
    I will finish the region allocators tomorrow, redo the library to make use of it and then complete the char RNN as per plan.
    
    I swear it upon my desire that it will be done by the end of the month.
    
    And then I will finally start work on reinforcement learning starting with multi armed bandits after which I will move to poker.
    
    I want to get to the point where I am already working on interfaces for online sites in mid April - in just two months.

--- Learning/Module.fs
+++ Learning/Module.fs
@@ -1,149 +1,51 @@
-﻿module Modules
+﻿module Learning.Modules
 
 open Spiral.Types
 open Spiral.Lib
 
-//let cuda_stream = 
-//    "CudaStream",[extern_],"The Cuda stream module.",
-//    """
-//inl {Cuda} ->
-//    open Extern
-//    inl ty x = fs [text: x]
-//    inl CudaStream_type = ty "ManagedCuda.CudaStream"
-//    inl CUstream_type = ty "ManagedCuda.BasicTypes.CUstream"
+let resize_array =
+    (
+    "ResizeArray",[extern_],"The resizable array module.",
+    """
+open Extern
+inl create {d with elem_type} = 
+    inl ty = fs [text: "ResizeArray"; types: elem_type]
+    inl x = 
+        match d with 
+        | {size} -> FS.Constructor ty (to int32 size)
+        | _ -> FS.Constructor ty ()
 
-//    inl create ret = 
-//        use stream = FS.Constructor CudaStream_type ()
-//        ret function
-//            | .extract -> extract stream
-//            | .get -> FS.Method x .get_Stream() CUstream_type 
-//            | .wait_on -> () // TODO
-            
-//    inl async ret = 
-//        inb stream = create
-//        inl r = ret stream
+    inl filter f = FS.Method x ."RemoveAll <| System.Predicate" (closure_of f (elem_type => bool)) int32 |> ignore
+    inl sort f =
+        inl comparison_type = fs [text: "System.Comparison"; types: elem_type]
+        inl f = closure_of f (elem_type => elem_type => int32)
+        inl c = FS.Constructor comparison_type f
+        FS.Method x .Sort c ()
 
-//    {create}
-//    """
+    inl index i = macro.fs elem_type [arg: x; text: ".["; arg: to int32 i; text: "]"]
+    inl set i v = macro.fs () [arg: x; text: ".["; arg: to int32 i; text: "] <- "; arg: v]
+    inl clear () = FS.Method x .Clear() ()
+    inl count () = FS.Method x .get_Count() int32
+    inl add y = FS.Method x .Add y ()
 
-//let cuda_tensor = 
-//    (
-//    "CudaTensor",[option;extern_;host_tensor],"The Cuda tensor module.",
-//    """
-//inl {current_region current_stream Cuda} ->
-//    open Cuda
-//    open HostTensor
-//    open Extern
-
-//    /// Is just a CUdeviceptr rather than the true array.
-//    inl array_create_cuda_global elem_type len = // TODO: Why does this diverge when `|> stack`ed.
-//        inl ptr = current_region (len * sizeof elem_type)
-//        function // It needs to be like this rather than a module so toa_map does not split it.
-//        | .elem_type -> elem_type
-//        | .ptr -> ptr
-//        |> stack
-//    inl create data = create {data with array_create = array_create_cuda_global}
-//    inl create_like tns = create {elem_type=tns.elem_type; dim=tns.dim}
-
-//    inl ptr_cuda {ar offset} ret = ar.ptr() + to uint64 (offset * sizeof ar.elem_type) |> ret
-//    inl CUResult_ty = fs [text: "ManagedCuda.BasicTypes.CUResult"]
-//    inl assert_curesult res = macro.fs unit [text: "if "; arg: res; text: " <> ManagedCuda.BasicTypes.CUResult.Success then raise <| new ManagedCuda.CudaException"; args: res]
-//    inl memcpy dst_ptr src_ptr size = macro.fs CUResult_ty [text: "ManagedCuda.DriverAPINativeMethods.SynchronousMemcpy_v2.cuMemcpy"; args: CUdeviceptr dst_ptr, CUdeviceptr src_ptr, SizeT size] |> assert_curesult
-
-//    inl GCHandle_ty = fs [text: "System.Runtime.InteropServices.GCHandle"]
-//    inl ptr_dotnet {ar offset} ret =
-//        inl elem_type = ar.elem_type
-//        inl handle = macro.fs GCHandle_ty [type:GCHandle_ty; text: ".Alloc"; parenth: [arg: ar; text: "System.Runtime.InteropServices.GCHandleType.Pinned"]]
-//        inl r =
-//            macro.fs int64 [arg: handle; text: ".AddrOfPinnedObject().ToInt64()"] 
-//            |> to uint64 |> (+) (to uint64 (offset * sizeof elem_type)) |> ret
-//        macro.fs unit [arg: handle; text: ".Free()"]
-//        r
-
-//    inl copy span dst {src with ar size ptr_get} =
-//        inl elem_type = ar.elem_type 
-//        assert (blittable_is elem_type) "The host array type must be blittable."
-//        inl span_size = match size with () -> span | size :: _ -> span * size
-//        inb src = ptr_get src
-//        inl memcpy dst = memcpy dst src (span_size * sizeof elem_type)
-//        match dst with
-//        | {ar size=size' ptr_get} -> 
-//            assert (size' = size) "The source and the destination must have the same sizes."
-//            assert (eq_type ar.elem_type elem_type) "The source and the destination must have the same types"
-//            inb dst = ptr_get dst
-//            memcpy dst
-//        | {array_create ptr_get} -> 
-//            inl ar = array_create elem_type span_size
-//            inb dst = ptr_get {ar offset=0}
-//            memcpy dst
-//            ar
-
-//    met from_host_array (!dyn span) (!dyn {src with ar offset size}) =
-//        copy span {array_create=array_create_cuda_global; ptr_get=ptr_cuda} {src with ptr_get=ptr_dotnet}
-
-//    met to_host_array (!dyn span) (!dyn {src with ar offset size}) =
-//        copy span {array_create ptr_get=ptr_dotnet} {src with ptr_get=ptr_cuda}
-
-//    inl get_elem {src with size=()} = to_host_array 1 src 0
-//    met set_elem (!dyn {dst with size=()}) (!dyn v) =
-//        inl ar = array_create v 1
-//        ar 0 <- v
-//        copy 1 {dst with ptr_get=ptr_cuda} {ar size=(); offset=0; ptr_get=ptr_dotnet}
-
-//    inl get tns = 
-//        match tns.unwrap with
-//        | {bodies dim=()} -> toa_map get_elem bodies
-//        | _ -> error_type "Cannot get from tensor whose dimensions have not been applied completely."
-
-//    inl set tns v = 
-//        match tns.unwrap with
-//        | {bodies dim=()} -> toa_iter2 set_elem bodies v
-//        | _ -> error_type "Cannot set to a tensor whose dimensions have not been applied completely."
-
-//    inl transfer_template f tns = 
-//        assert_contiguous tns
-//        inl f = tns.dim |> fst |> span |> f
-//        tns.update_body <| inl body -> {body with ar = f body}
-
-//    inl from_host_tensor tns = transfer_template from_host_array tns
-//    inl to_host_tensor tns = transfer_template to_host_array tns
-//    inl to_dev_tensor tns = tns.update_body (inl body -> 
-//        inb ptr = ptr_cuda body
-//        {body with ar=!UnsafeCoerceToArrayCudaGlobal(ptr,body.ar.elem_type); offset=0}
-//        )
-
-//    inl clear tns = 
-//        assert_contiguous tns
-//        inl span = tns.dim |> fst |> span
-//        inl stream = current_stream .extract
-//        tns.update_body <| inl {body with size=size::_ ar} ->
-//            FS.Method context .ClearMemoryAsync (CUdeviceptr (ar.ptr()),0u8,size * span * sizeof ar.elem_type |> SizeT,stream) unit
-//        |> ignore
-
-//    inl clear' x = clear x; x
-//    inl zero = create >> clear'
-//    inl zero_like = create_like >> clear'
-
-//    inl from_host_tensors x ret = 
-//        inl tensors = toa_map from_host_tensor x
-//        inl r = ret tensors
-//        toa_map (inl x -> x.update_body (inl {ar} -> ar.ptr.Dispose)) |> ignore
-//        r
-
-//    met print (!dyn o1) = to_host_tensor o1 |> HostTensor.print
-
-//    // CPS'd variants of the allocator functions.
-//    inl create = safe_alloc 1 create
-//    inl from_host_tensor = safe_alloc 1 from_host_tensor
-//    inl zero = safe_alloc 1 zero
-//    inl zero_like = safe_alloc 1 zero_like
-
-//    {create from_host_tensor from_host_tensors to_host_tensor to_dev_tensor clear zero zero_like print get set}
-//    """) |> module_
+    function
+    | .sort -> sort 
+    | .filter -> filter 
+    | .set i v -> set i v
+    | .clear -> clear ()
+    | .count -> count ()
+    | .add -> add
+    | .elem_type -> elem_type
+    | i -> index i
+{ 
+create
+} |> stack
+    """
+    ) |> module_
 
 let allocator = 
     (
-    "Allocator",[loops;option;extern_;console],"The section based GPU memory allocator module.",
+    "Allocator",[resize_array;loops;option;extern_;console],"The section based GPU memory allocator module.",
     """
 inl {Cuda} size ->
     open Cuda
@@ -166,67 +68,54 @@ inl {Cuda} size ->
         to uint64 >> round_up_to_multiple >> dyn
         >> inl size -> { size ptr = FS.Method context .AllocateMemory (SizeT size) CUdeviceptr_type |> to_uint |> smartptr_create }
 
-    inl resize_type = fs [text: "ResizeArray"; types: pool]
-    inl resize_array_filter f ar = FS.Method ar .RemoveAll (closure_of f (pool => int32)) int32 |> ignore
-    inl resize_array_sort f ar =
-        inl comparison_type = fs [text: System.Comparison]
-        inl f = closure_of f (pool => pool => int32)
-        inl c = FS.Contructor comparison_type f
-        FS.Method ar .Sort c unit
-
     inl compare a b = if a < b then -1i32 elif a = b then 0i32 else 1i32
-    inl index free_cells i = macro.fs pool [arg: free_cells; iter: ".[","","]",[arg: to int32 i]]
-    inl set free_cells i v = macro.fs () [arg: free_cells; iter: ".[","","] <- ",[arg: to int32 i]; arg: v]
-
-    inl filter_empty = resize_array_filter (inl {ptr} -> ptr.Try = 0u64)
-    inl sort_ptrs = resize_array_sort (inl {ptr=a} {ptr=b} -> compare (a()) (b()))
+    inl sort_ptrs x = x.sort (inl {ptr=a} {ptr=b} -> compare (a()) (b()))
 
     met free_cells_refresh {section with pool free_cells used_cells} = 
-        filter_empty used_cells; sort_ptrs used_cells
-        FS.Method free_cells .Clear() ()
-        inl near_to = FS.Method used_cells .get_Count() int32
-        inl add {ptr size} = FS.Method free_cells .Add {ptr = smartptr_create (ptr()); size} ()
-        inl index = index free_cells
+        used_cells.filter (inl {ptr} -> ptr.Try = 0u64)
+        sort_ptrs used_cells
+        free_cells.clear
+        inl near_to = used_cells.count
+        inl add {ptr size} = free_cells.add {ptr = smartptr_create (ptr()); size}
 
         inl distance state state' = 
             inl p1 = state.ptr() + state.size
             inl p2 = state.ptr()
             p2 - p1
             
-        Loops.for {from=0i32; near_to by=1i32; state={pool with size = 0u64}; body=inl {state i} ->
-            inl state' = index i
+        Loops.for {from=0i32; near_to by=1i32; state={pool with size = 0u64}; body=inl {{state with ptr} i} ->
+            inl state' = free_cells i
             inl size = distance state state'
             if size > 0u64 then add { ptr size }
             state'
             }
-        |> inl state = // This is for the final free cell at the end.
+        |> inl state -> // This is for the final free cell at the end.
             inl size = distance pool state
             add {state with size}
 
-    met allocate {section with free_cells} (!dyn size') =
-        inl index = index free_cells
-        inl set = set free_cells
-        inl near_to = FS.Method free_cells .get_Count() int32
+    inl s = to uint64 >> dyn
+    met allocate {section with free_cells} (!s size') =
         inl loop next = 
-            inl i = 0i32
-            inl {ptr size} = index i
+            inl {ptr size} = free_cells 0i32
             if size' <= size then 
-                set i {ptr=smartptr_create (ptr.Try+size'); size=size-size'}
+                free_cells.set 0i32 {ptr=smartptr_create (ptr.Try+size'); size=size-size'}
                 {ptr size=size'}
             else next()
 
-        loop <| inl _ ->
-            sort_ptrs free_cells
-            loop <| inl _ -> 
-                free_cells_refresh section
+        inl {ptr} =
+            loop <| inl _ ->
                 sort_ptrs free_cells
-                loop <| inl _ ->
-                    failwith () "Out of memory in the designated section."
+                loop <| inl _ -> 
+                    free_cells_refresh section
+                    sort_ptrs free_cells
+                    loop <| inl _ ->
+                        failwith free_cells.elem_type "Out of memory in the designated section."
+        ptr
 
     inl create_section size =
-        inl free_cells = FS.Constructor resize_type ()
-        inl used_cells = FS.Constructor resize_type ()
         inl pool = allocate_global size
+        inl elem_type = type pool
+        inl free_cells, used_cells = ResizeArray.create {elem_type}, ResizeArray.create {elem_type}
         inl section = {pool free_cells used_cells}
         free_cells_refresh section
         function
---
+++ Learning/Tests.fs
@@ -0,0 +1,34 @@
+﻿module Learning.Tests
+
+open Spiral.Lib
+open Spiral.Tests
+open System.IO
+open Spiral.Types
+open Modules
+
+let allocator1 =
+    "allocator1",[allocator;cuda],"Does the allocator work?",
+    """
+inb Cuda = Cuda
+inl allocate = Allocator {Cuda} 1024
+inl a = allocate 128
+inl b = allocate 64
+inl c = allocate 32
+c.Dispose
+b.Dispose
+a.Dispose
+()
+    """
+
+let cfg: Spiral.Types.CompilerSettings = {
+    path_cuda90 = "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v9.0"
+    path_cub = "C:/cub-1.7.4"
+    path_vs2017 = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community"
+    cuda_includes = ["cub/cub.cuh"]
+    trace_length = 40
+    cuda_assert_enabled = false
+    }
+    
+output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" allocator1
+|> printfn "%s"
+|> ignore
