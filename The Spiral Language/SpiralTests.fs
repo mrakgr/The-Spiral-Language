99165a61c54616bf69d36f483f123257c2eb104d
Marko Grdinić <mrakgr@gmail.com>
2018-02-26 12:57:58 +0100

    Well, that is it for the new patterns and their contructors. Maybe I'll later extend the language so $ can be used on the left side of the module definition, but it is not a priorty right now.
    Let me see if I can contruct and run the net.

--- The Spiral Language/SpiralTests.fs
+++ The Spiral Language/SpiralTests.fs
@@ -1157,6 +1157,16 @@ inl f i {$i=x} = x
 f .a m, f .b m
     """
 
+let test107 = 
+    "test107",[],"Does the injection constructor work?",
+    """
+inl f i v m = {m with $i=v}
+{}
+|> f .a 123
+|> f .b 456
+|> inl {a b} -> a,b
+    """
+
 let parsing1 = 
     "parsing1",[parsing;console],"Does the Parsing module work?",
     """
@@ -1942,7 +1952,7 @@ let tests =
     test70;test71';test72;test73;test74;test75;test76';test77';test78;test79
     test80;test81;test82;test83;test84;test85;test86;test87;test88;test89
     test90;test91;test92;test93;test94;test95;test96;test97;test98;test99
-    test100;test101;test102;test103;test104;test105
+    test100;test101;test102;test103;test104;test105;test106;test107
     hacker_rank_1;hacker_rank_2;hacker_rank_3;hacker_rank_4;hacker_rank_5;hacker_rank_6;hacker_rank_7;hacker_rank_8;hacker_rank_9
     parsing1;parsing2;parsing3;parsing4;parsing5;parsing6;parsing7;parsing8
     loop1;loop2;loop3;     loop5;loop6;loop7;loop8
