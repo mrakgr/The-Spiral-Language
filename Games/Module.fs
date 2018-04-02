2174eb0a950177f2fdb979c55a39be997920a28e
Marko Grdinić <mrakgr@gmail.com>
2018-04-02 11:24:24 +0200

    ``` inl one_card_poker state players =     match phase with     | .Dealing ->         inl f ante player state =             if player.chips >= ante then                 inl card, state = state .pot_add ante .deck.take                 player.chips_remove ante .hand_set card, state             else player.inactive, state
            Tuple.foldr_map (inl player {ante state} ->
                inl player,state = f ante player state
                player, {ante=1; state}
                ) players {ante=2; state}
    ```
    
    I really got engrossed in the last hour. I dredged up a few memories at first of some my past regrets, then I went forward with the design. AFter I wrote this, I started thinking about what the deck should be. I am starting to think it should be mutable rather than a list. I do not want to waste cycles here.
    
    At first I thought of going functional, but you know, I think after all that I am going to go with a full out OO design. A generic OO design like a language like Smalltalk would offer, not the C# garbage.
    
    Let me redesign this and then I'll have breakfast.
    
    One thing I am sure that this is not how I approached it last time. Now only do I have Spiral now, but the way I look at programming has fundamentally shifted. It is really simplicity itself now. I do not need anything other than this.

--- Games/Module.fs
+++ Games/Module.fs
@@ -8,8 +8,22 @@ let poker =
     (
     "Poker",[],"The Poker module",
     """
+/// The bot that just predictably goes all in.
 inl aggrobot state = .AllIn
 
-inl response_raise player x =
-    player.reply (.Raise, x)
+/// The controller for the one card poker game.
+inl one_card_poker state players =
+    match phase with
+    | .Dealing -> 
+        inl f ante player state =
+            if player.chips >= ante then 
+                inl card, state = state .pot_add ante .deck.take
+                player.chips_remove ante .hand_set card, state
+            else player.inactive, state
+
+        Tuple.foldr_map (inl player {ante state} -> 
+            inl player,state = f ante player state
+            player, {ante=1; state}
+            ) players {ante=2; state}
+
     """) |> module_
