063bc530dd196f4bf250677564368b6439273466
Marko Grdinić <mrakgr@gmail.com>
2018-04-02 10:31:11 +0200

    https://www.reddit.com/r/reinforcementlearning/comments/87nya1/world_models_can_agents_learn_inside_their_own/?st=jfhym1li&sh=e1bee69c
    I admit I got distracted a little by this. It is quite impressive. I saw such game halucination videos before by Graves, but this is the first time a RL agents was trained in such an environment. I'll read the paper later.
    
    Let me do some actual work now.

--- Games/Module.fs
+++ Games/Module.fs
@@ -1,1 +1,18 @@
-﻿
+﻿module Poker.Module
+
+open Spiral.Types
+open Spiral.Lib
+open Learning.Module
+
+let poker =
+    (
+    "Poker",[],"The Poker module",
+    """
+//inl Player chips =
+//    {
+//    chips
+//    hand
+//    }
+
+//inl Game players = 
+    """) |> module_
