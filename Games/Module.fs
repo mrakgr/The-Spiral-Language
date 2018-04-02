eac814978a5d8294be28900e67af2f1b2326ab59
Marko Grdinić <mrakgr@gmail.com>
2018-04-02 11:41:00 +0200

    This is how programming power manifests - as simplicity. I am going to have to extend the parser for this, but this is just wonderful.
    Once I decided that I wanted OO architecture, it just came to me on its own. Truly wonderful. Had I been torn between different approaches I could not have thought of it.
    
    Let me take a break here. I need to eat.
    
    I'll sketch out the program after that.

--- Games/Module.fs
+++ Games/Module.fs
@@ -13,17 +13,14 @@ inl aggrobot state = .AllIn
 
 /// The controller for the one card poker game.
 inl one_card_poker state players =
-    match phase with
+    match state.phase with
     | .Dealing -> 
-        inl f ante player state =
+        inl f ante player =
             if player.chips >= ante then 
-                inl card, state = state .pot_add ante .deck.take
-                player.chips_remove ante .hand_set card, state
-            else player.inactive, state
+                inl card = state [pot.add: ante; deck.take]
+                player [bet: ante; hand.set: card]
+            else player.inactive
 
-        Tuple.foldr_map (inl player {ante state} -> 
-            inl player,state = f ante player state
-            player, {ante=1; state}
-            ) players {ante=2; state}
+        Tuple.foldr (inl player ante -> f ante player; 1) players 2
 
     """) |> module_
