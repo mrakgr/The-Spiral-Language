0789b5ddb642a83d1788900bcd6538db562826ce
Marko Grdinić <mrakgr@gmail.com>
2018-04-09 16:17:02 +0200

    Adding destructuring into the join point does nothing. One my assumptions is definitely broken.
    Well, I'll continue paring it down.

--- Games/Tests.fs
+++ Games/Tests.fs
@@ -16,38 +16,6 @@ let cfg: Spiral.Types.CompilerSettings = {
     cuda_assert_enabled = false
     }
 
-let poker1 =
-    "poker1",[poker],"Does the poker game work?",
-    """
-open Poker Console.printfn
-one_card 5 ({reply=reply_random; name="One"}, {reply=reply_random; name="Two"})
-|> ignore
-    """
-
-let poker2 =
-    "poker2",[poker],"Does the rules based player work?",
-    """
-open Poker Console.printfn
-one_card 5 ({reply=reply_rules; name="One"}, {reply=reply_random; name="Two"})
-|> ignore
-    """
-
-let poker3 =
-    "poker3",[loops;poker],"What is the winrate of the rules based players against the random one?",
-    """
-inl log _ _ = ()
-open Poker log
-Loops.for {from=0; near_to=10000; state=dyn {a=0; b=0}; body=inl {state=s i} ->
-    inl a,b = one_card 6 ({reply=reply_rules; name="One"}, {reply=reply_random; name="Two"})
-    match a.name with
-    | "One" -> if a.chips > 0 then {s with a=self+1} else {s with b=self+1}
-    | _ -> if a.chips > 0 then {s with b=self+1} else {s with a=self+1}
-    }
-|> inl {a b} ->
-    inl total = a + b
-    Console.printfn "Winrate is {0} and {1} out of {2}." (a,b,total)
-    """
-
 let poker4 =
     "poker4",[loops;poker;timer],"What is the winrate of the RL based players against the random one?",
     """
@@ -55,21 +23,9 @@ inl log _ _ = ()
 open Poker log
 inl a = {reply=reply_q {init=5f64; learning_rate=0.03f64; num_players=2}; name="One"; trace=term_cast (inl _ -> ()) float64}
 inl b = {reply=reply_random; name="Two"}
-Loops.for {from=0; near_to=100; body=inl {i} ->
-    Timer.time_it (string_format "iteration {0}" i)
-    <| inl _ ->
-        Loops.for {from=0; near_to=10000; state=dyn {a=0; b=0}; body=inl {state=s i} ->
-            inl a,b = one_card 10 (a, b)
-            match a.name with
-            | "One" -> if a.chips > 0 then {s with a=self+1} else {s with b=self+1}
-            | _ -> if a.chips > 0 then {s with b=self+1} else {s with a=self+1}
-            }
-        |> inl {a b} ->
-            inl total = a + b
-            Console.printfn "Winrate is {0} and {1} out of {2}." (a,b,total)
-    }
+one_card 10 (a, b) |> ignore
     """
 
 output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" poker4
-//|> printfn "%s"
+|> printfn "%s"
 |> ignore
