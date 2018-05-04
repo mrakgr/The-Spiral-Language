407de3ab7ae7182f3187a6ac0a2c6232b03b3619
Kevin Malenfant <kevin@xaai.ch>
2018-05-04 15:31:37 -0600

    abs path to relative, fsi friendly

--- Testing/run.fs
+++ Testing/run.fs
@@ -1,4 +1,11 @@
-﻿module File1
+#if INTERACTIVE
+#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsecCs.dll"
+#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsec.dll"
+#r @"..\packages\ManagedCuda-CUBLAS.8.0.22\lib\net46\CudaBlas.dll"
+#r @"..\packages\ManagedCuda-CURAND.8.0.22\lib\net46\CudaRand.dll"
+#r @"Microsoft.CSharp"
+#r @"..\The Spiral Language\bin\Debug\The_Spiral_Language.dll"
+#endif
 
 open Spiral.Lib
 open Spiral.Tests
@@ -25,7 +32,7 @@ f (dyn 1) (dyn 2)
     """
 
 //rewrite_test_cache tests cfg None //(Some(0,40))
-output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" example
+output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__ , @"..\Temporary\output.fs")) example
 |> printfn "%s"
 |> ignore
 
