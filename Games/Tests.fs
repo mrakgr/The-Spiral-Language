12b9d1d5d2ce7d9f3d2412de0e6051934faea0cf
Marko Grdinić <mrakgr@gmail.com>
2018-05-04 09:12:47 +0200

    It does seem to be working on the simpler tests.

--- Games/Tests.fs
+++ Games/Tests.fs
@@ -269,6 +269,6 @@ Loops.for {from=0; near_to=10; body=inl {i} ->
     }
     """
 
-output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" serializer6
+output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" poker5
 |> printfn "%s"
 |> ignore
