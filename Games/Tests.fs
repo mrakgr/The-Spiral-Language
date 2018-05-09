4d65ee46df5370f9c5828906481fb1a5f89e699b
Marko Grdinić <mrakgr@gmail.com>
2018-05-09 09:27:39 +0200

    "8:40am. Yeah, nothing beats going to bed when you are not overstrained. When my stress is high I dream about going back to school and have a nightmare about missing the bus or getting on the wrong one. Today I dreamed about killing demons like I was playing Doom. I much prefer this type of dream.
    Now...I am looking at the previous commit thinking what I will do today.
    
    9am. I guess chilling a bit would not be bad. But basically, I am thinking of getting RNNs operational today. I want to see whether they will make any difference with MC updates.
    
    I am really thinking of what will come after. I want to switch to doing TD learning eventually, and even though I know it will be unstable, I want to check out how Q learning in particular will work or whether it will be irredeemably unstable. This is without target networks. I want to throw the full course at making it work.
    
    9:10am. There is no hurrying this. Here I clearly must experience failure in order to form a basis for the correct steps down the road. I need to do exploration at this particular juncture.
    
    So here will be my goals.
    
    1) Get RNNs operational.
    2) Try out deep Q learning with both squared error and quantile regression.
    
    9:15am. There will be a bit of work involved in getting this to work.
    
    One of the things I am wondering about for when I try out TD learning is whether I should propagate gradients through the targets or not?
    
    9:20am. I wrote what I am going to do, but it is surprisingly difficult to put the feeling into words. At any rate, I have my sense of direction.
    
    9:25am. I'll take it easy as I go along. I am going to paste this excerpt from the journal into the commit, but strangely enough I still haven't started chilling. Let me just stop this.
    
    I'll only focus on getting RNNs operational for today. Maybe I will throw in the other one if I manage to find the time."

--- Games/Tests.fs
+++ Games/Tests.fs
@@ -335,4 +335,3 @@ Loops.for {from=0; near_to=20; body=inl {i} ->
 output_test_to_temp cfg @"C:\Users\Marko\Source\Repos\The Spiral Language\Temporary\output.fs" poker7
 |> printfn "%s"
 |> ignore
-
