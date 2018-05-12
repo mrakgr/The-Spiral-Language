9b9b717cadcf2af688e8649e56d8b0f051678db6
Marko Grdinić <mrakgr@gmail.com>
2018-05-12 12:45:00 +0200

    Will this be fine? I guess I will try running it. Also this will do more frequent updates that the other version. Will that be fine as well?
    Well, I'll compare it later.
    
    For now let me just get this compile and I will think about what comes after that.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -45,7 +45,7 @@ f 10 1000
     """
 
 let poker5 =
-    "poker5",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (square error feedforward) player against the random one?",
+    "poker5",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (square error) player against the random one?",
     """
 inb s = CudaModules (1024*1024*1024)
 inl log _ _ = ()
@@ -72,7 +72,7 @@ Loops.for {from=0; near_to=10; body=inl {i} ->
     """
 
 let poker6 =
-    "poker6",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (QR feedforward) player against the random one?",
+    "poker6",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (QR) player against the random one?",
     """
 inb s = CudaModules (1024*1024*1024)
 inl log _ _ = ()
@@ -99,7 +99,7 @@ Loops.for {from=0; near_to=10; body=inl {i} ->
     """
 
 let poker7 =
-    "poker7",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (KL feedforward) player against the random one?",
+    "poker7",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC learning (KL) player against the random one?",
     """
 inb s = CudaModules (1024*1024*1024)
 inl log _ _ = ()
@@ -115,6 +115,32 @@ Loops.for {from=0; near_to=10; body=inl {i} ->
             inb s = s.RegionMem.create'
             inl a,b = one_card stack_size ({a' with reply=self s |> heap}, b')
             a'.optimize 0.001f32 s
+            match a.name with
+            | "One" -> if a.chips > 0 then {state with a=self+1} else {state with b=self+1}
+            | _ -> if a.chips > 0 then {state with b=self+1} else {state with a=self+1}
+            }
+        |> inl {a b} ->
+            inl total = a + b
+            Console.printfn "Winrate is {0} and {1} out of {2}." (a,b,total)
+    }
+    """
+
+let poker8 =
+    "poker8",[cuda_modules;loops;poker;timer],"What is the winrate of the deep MC meta-learning (square error) player against the random one?",
+    """
+inb s = CudaModules (1024*1024*1024)
+inl log _ _ = ()
+open Poker log
+inl stack_size = 10
+inl a' = reply_meta_dmc {bias=0.0; scale=to float64 stack_size; range=32; num_players=2; name="One"; learning_rate=0.01f32} s
+inl b' = reply_random {name="Two"}
+Loops.for {from=0; near_to=10; body=inl {i} ->
+    Timer.time_it (string_format "iteration {0}" i)
+    <| inl _ ->
+        Loops.for {from=0; near_to=1000; state=dyn {a=0; b=0}; body=inl {state i} ->
+            s.refresh
+            inb s = s.RegionMem.create'
+            inl a,b = one_card stack_size ({a' with reply=self s |> heap}, b')
             match a.name with
             | "One" -> if a.chips > 0 then {state with a=self+1} else {state with b=self+1}
             | _ -> if a.chips > 0 then {state with b=self+1} else {state with a=self+1}
