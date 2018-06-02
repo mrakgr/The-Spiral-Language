d1554e9aad46ac5a4a8a10d6d8c1814bb842f883
Marko Grdinić <mrakgr@gmail.com>
2018-06-02 08:31:41 +0200

    Somehow I did not feel like reading where I left of yesterday so I ended up watching a lecture on variational inference.
    It sort of ends up being policy gradient...
    
    The natural gradient part of it only really concerns the update to single variables which makes it similar to relative gradient for single unit groupings.
    
    Meh, the Bayesian stuff always boggles my mind. The Bayesian methods are going to turn into NNs by the end of all of this.
    
    ...
    
    Forget that for a while. Right now what is on the agenda is to deal with the issue. Here I've factored one the options into a variable. Let me make sure everything still works and then I will turn them into parser macros and extend the record.
    
    It is best to do it in two steps like this.
    
    After I am done with the issue, I'll get back to reading. I fired a mail to Ke Sun asking him about whether he tried replacing the matrix inverse with SGD, so hopefully I should get a reply by tomorrow or in the next few days.
    
    There is no point in starting work on RNG until that crucial bit is cleared out.
    
    Now let me get this out of the way.

--- The Spiral Language/SpiralLib.fs
+++ The Spiral Language/SpiralLib.fs
@@ -1626,6 +1626,11 @@ inl ret ->
     inl cuda_toolkit_path = @PathCuda
     inl visual_studio_path = @PathVS2017
     inl cub_path = @PathCub
+    inl nvcc_options_target = "-gencode=arch=compute_52,code=\\\"sm_52,compute_52\\\""
+    inl vs_path_vcvars = "VC/Auxiliary/Build/vcvarsall.bat"
+    inl vcvars_args = " x64 -vcvars_ver=14.11"
+    inl vs_path_cl = "VC/Tools/MSVC/14.11.25503/bin/Hostx64/x64"
+    inl vs_path_include = "VC/Tools/MSVC/14.11.25503/include"
 
     inl env_type = fs [text: "System.Environment"]
     inl context_type = fs [text: "ManagedCuda.CudaContext"]
@@ -1663,11 +1668,11 @@ inl ret ->
 
         /// Puts quotes around the string.
         inl quote x = ("\"",x,"\"")
-        inl vc_vars_args = " x64 -vcvars_ver=14.11"
-        inl quoted_vs_path_to_vcvars = combine(visual_studio_path, "VC/Auxiliary/Build/vcvarsall.bat") |> quote
-        inl quoted_vs_path_to_cl = combine(visual_studio_path, "VC/Tools/MSVC/14.11.25503/bin/Hostx64/x64") |> quote
+
+        inl quoted_vs_path_to_vcvars = combine(visual_studio_path, vs_path_vcvars) |> quote
+        inl quoted_vs_path_to_cl = combine(visual_studio_path, vs_path_cl) |> quote
+        inl quoted_vc_path_to_include = combine(visual_studio_path, vs_path_include) |> quote
         inl quoted_cuda_toolkit_path_to_include = combine(cuda_toolkit_path,"include") |> quote
-        inl quoted_vc_path_to_include = combine(visual_studio_path, "VC/Tools/MSVC/14.11.25503/include") |> quote
         inl quoted_nvcc_path = combine(cuda_toolkit_path,@"bin/nvcc.exe") |> quote
         inl quoted_cub_path_to_include = cub_path |> quote
         inl quoted_kernels_dir = kernels_dir |> quote
@@ -1689,10 +1694,10 @@ inl ret ->
             inl write_to_batch = concat >> inl x -> FS.Method nvcc_router_stream .WriteLine x ()
 
             "SETLOCAL" |> write_to_batch
-            ("CALL ", quoted_vs_path_to_vcvars, vc_vars_args) |> write_to_batch
+            ("CALL ", quoted_vs_path_to_vcvars, vcvars_args) |> write_to_batch
             ("SET PATH=%PATH%;", quoted_vs_path_to_cl) |> write_to_batch
             (
-            quoted_nvcc_path, " -gencode=arch=compute_52,code=\\\"sm_52,compute_52\\\" --use-local-env --cl-version 2017",
+            quoted_nvcc_path, ," ", nvcc_options_target, " --use-local-env --cl-version 2017",
             " -I", quoted_cuda_toolkit_path_to_include,
             " -I", quoted_cub_path_to_include,
             " -I", quoted_vc_path_to_include,
