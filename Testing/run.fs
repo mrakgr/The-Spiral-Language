0741959acdb9c97ad8c0da2e9712e44b8a26c34b
Marko Grdinić <mrakgr@gmail.com>
2018-07-19 08:54:03 +0200

    Here is how the Julia example could be done in Spiral.

--- Testing/run.fs
+++ Testing/run.fs
@@ -17,11 +17,29 @@ let cfg = Spiral.Types.cfg_testing
 let example = 
     "example",[],"Module description.",
     """
-inl f a b =
-    inl c = a + b
-    c + 3
+// https://gist.github.com/SimonDanisch/812e01d7183681ed414932c8f7b533d0#file-unroll-jl
+// This is not quite the same as the above example, but Spiral's staging optimizations
+// only work when code is written in a functionally pure manner. 
 
-f (dyn 1) (dyn 2)
+// `sin` currently is not a part of the core library so I use a macro to interop with it.
+// If it were then this whole program would be evaluated at compile time.
+// To see that, replace sin with tanh or sqrt for example.
+// Spiral's semantics allow for any functionally pure computation at compile time.
+inl sin x = macro.fs float64 [text:"sin"; args: x]
+
+inl rec unroll f i s =
+    assert (lit_is i) "i must be a literal."
+    if i > 0 then unroll f (i-1) (f s) else s
+
+inl test () =
+    inl box = 1.0
+    unroll sin 3 box
+
+test ()
+
+//let (var_0: float) = sin(1.000000)
+//let (var_1: float) = sin(var_0)
+//sin(var_1)
     """
 
 //rewrite_test_cache tests cfg None //(Some(0,40))
