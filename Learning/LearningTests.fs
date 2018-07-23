5c9cf3e075e2a7fdcc358a63f64f17d08f1d39d8
Marko Grdinić <mrakgr@gmail.com>
2018-07-23 09:53:45 +0200

    I am printing out the back whitening and it just grows until it gets clipped. What the hell? This is with the the global learning rate set to zero.
    I've been hopeful regarding Sherman Morrison, but I do not think I've implemented it incorrectly here. Is it just too unstable on its own?
    
    Let me make a covariance matrix so I can compare the two. Thankfully the size in the last dimension is just 10.

--- Learning/LearningTests.fs
+++ Learning/LearningTests.fs
@@ -812,7 +812,7 @@ Loops.for' {from=0; near_to=1;body=inl {next} ->
             data={input=train_images; label=train_labels}
             body=train {
                 network=network.train
-                //optimizer=Optimizer.sgd 0.01f32
+                optimizer=Optimizer.sgd 0.0f32
                 }
             } s
 
