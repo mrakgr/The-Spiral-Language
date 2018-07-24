c8d48ac79b240e73e073520193d4db09e07a6e80
Marko Grdinić <mrakgr@gmail.com>
2018-07-24 12:49:19 +0200

    I am done with iterative updates.
    The weighted Jacobi iteration does not work. I knew that all these methods had flaws, but I was hoping they would work well in practice. Unfortunately they do not.
    
    Let me make a tally.
    
    1) RNG L2 update - does not work.
    2) Inverse Cholesky update - does not work.
    3) Sherman Morrison update - does not work.
    4) Weighted Jacobi iteration - does not work.
    
    What a waste of time the last month and a half was. I should have saved myself 5h today and simply did what I said I would in last night's commit which was to bring in the direct inverse.
    
    Ultimately, these updates are supposed to be the most stable part of the whole architecture, but they just keep dying even on toy examples.
    
    This is no way to do ML.
    
    I'll put the code for this on a separate branch and then start from a clean slate.

--- Learning/LearningTests.fs
+++ Learning/LearningTests.fs
@@ -1062,22 +1062,42 @@ inl x =
     else
         x
 
-inl C = s.CudaBlas.gemm .T .nT (to float32 k) x x
+inl C = s.CudaBlas.gemm .T .nT (one / to float32 k) x x
+inl D,R = 
+    inl C = CudaAux.to_dev_tensor C
+    inl D = s.CudaKernel.init {dim=n} (inl i -> C i i .get)
+    inl R = s.CudaKernel.init {dim=n,n} (inl a b -> if a <> b then C a b .get else zero)
+    D,R
 
 inl A = s.CudaKernel.init {dim=n,n} (inl a b -> if a = b then one else zero)
+inl I = s.CudaKernel.init {dim=n,n} (inl a b -> if a = b then one else zero)
 
+Console.writeline "Inputs:"
 s.CudaTensor.print x
-s.CudaTensor.print C
 
-Console.writeline "---"
+Console.writeline "Covariances:"
 
-inl jacobi {w} s {D R} X B =
+s.CudaTensor.print D
+s.CudaTensor.print R
+
+inl jacobi s {D R w epsilon} X B =
     inb RX = s.CudaBlas.gemm .nT .nT one R X |> CudaAux.temporary
-    s.CudaBlas.d2_replicate_map (inl D {RX B} X ->
-        w * (B - RX) / D + (one - w) * X
+    s.CudaKernel.d2_replicate_map' (inl D {RX B} X ->
+        w * (B - RX) / (D + epsilon) + (one - w) * X
         ) D {RX B} X
 
+Loops.for {from=0; near_to=500; body=inl {i} ->
+    Loops.for {from=0; near_to=50; body=inl {i} ->
+        jacobi s {D R w=0.0001f32; epsilon=0.00001f32} A I
+        }
+    Console.printfn "At iteration {0}:" i
+    //Console.writeline "Inverse:"
+    //s.CudaTensor.print A
 
+    Console.writeline "C * C^-1:"
+    inb T = s.CudaBlas.gemm .nT .nT one C A |> CudaAux.temporary
+    s.CudaTensor.print T
+    }
 
     """
 
@@ -1093,12 +1113,11 @@ let tests =
     cusolver1
     learning1;learning2;learning3;learning4;learning5;                               learning9
     learning10;learning11
-    inverse1;inverse2;inverse3
     prong1
     |]
 
 //rewrite_test_cache tests cfg None //(Some(0,40))
 
-output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) prong1
+output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) jacobi1
 |> printfn "%s"
 |> ignore
