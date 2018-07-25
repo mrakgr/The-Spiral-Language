3276bae92546dbb6d4e5095904549460f5353860
Marko Grdinić <mrakgr@gmail.com>
2018-07-25 16:30:04 +0200

    ``` 21536833536 21536833792 21536834048 21536833792 21536834048 ```
    It does seem like it works. Let me try the `learning9` and `learning10`.

--- Learning/LearningTests.fs
+++ Learning/LearningTests.fs
@@ -1090,7 +1090,7 @@ let tests =
 
 //rewrite_test_cache tests cfg None //(Some(0,40))
 
-output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) allocator1
+output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) learning9
 |> printfn "%s"
 |> ignore
 
