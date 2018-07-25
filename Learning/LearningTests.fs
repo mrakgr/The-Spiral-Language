6c9e4d73efc4239ecc06b41d76a3af143491d68b
Marko Grdinić <mrakgr@gmail.com>
2018-07-25 16:43:25 +0200

    The feedforward layers do it properly, but the recurrent errors have slight deviations from run to run that are not related to atomics.
    Let me take a look at the layers more carefully.

--- Learning/LearningTests.fs
+++ Learning/LearningTests.fs
@@ -886,7 +886,8 @@ inl network =
     inl train =
         input
         //|> miln 0.05f32 128
-        |> mi 128
+        //|> mi 128
+        //|> sigmoid 128
         |> Feedforward.Layer.linear size.hot
         |> error Error.softmax_cross_entropy label
         |> init_parallel s
