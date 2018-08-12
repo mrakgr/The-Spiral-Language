c4685da6e2b7fc54714a2fd120fd3087e7024246
Marko Grdinić <mrakgr@gmail.com>
2018-08-12 11:55:40 +0200

    Let me see if this thing compiles. Maybe instead of initing to positive values, I'd be better off going with e-greedy, but forget that for now.
    To start things off, as usual, let me go through the type errors. I'll leave tuning the hyperparams for later.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -148,7 +148,7 @@ f game 30 100000
     """
 
 let poker3 =
-    "poker3",[cuda_modules;loops;poker;poker_players;timer],"The iterative test for NNs.",
+    "poker3",[cuda_modules;loops;poker;poker_players;timer],"The iterative test for NN based players.",
     """
 inb s = CudaModules (1024*1024*1024)
 inl num_players = 2
@@ -157,16 +157,17 @@ inl max_stack_size = num_players * stack_size
 open Poker {max_stack_size num_players}
 open PokerPlayers {basic_methods State Action}
 
-Loops.for {from=0; near_to=20; body=inl {i} ->
+Loops.for {from=0; near_to=1; body=inl {i} ->
     inb s = s.RegionMem.create'
     s.CudaRandom.set_pseudorandom_seed (to uint64 i)
 
     Console.writeline "------"
     Console.printfn "The CudaRandom pseudorandom seed is {0}" i
 
-    inl a = 
-        open (Learning float32).Feedforward
-        player_pg {name="One"; actor=tanh 256; learning_rate=0.0003f32} s
+    //inl a = 
+    //    open (Learning float32).Feedforward
+    //    player_pg {name="One"; actor=tanh 256; learning_rate=0.0003f32} s
+    inl a = player_zap_q {name="One"; init=3f32; steps_until_inverse_update=128; learning_rate=0.01f32; discount_factor=0.99f32} s
     inl b = player_rules {name="Two"}
 
     met f game (!dyn near_to) (!dyn near_to_inner) = 
