f687c7db22dc4e152c0d14f8e5f19bfbe64731ee
Marko Grdinić <mrakgr@gmail.com>
2018-08-15 16:40:36 +0200

    As the last thing for the day, what I will do is at least get the type errors out of the way. That will clear the path for me tomorrow. No doubt I will need to tune it and fix whatever runtime bugs come up.
    ```
        inl a =
            inl learning_rate = {actor=0.001f32; critic=0.001f32; shared=0.001f32}
            player_zap_ac {name="One"; learning_rate discount_factor=0.99f32; steps_until_inverse_update=128} s
    ```
    
    Let me give this a shot.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -164,10 +164,12 @@ Loops.for {from=0; near_to=1; body=inl {i} ->
     Console.writeline "------"
     Console.printfn "The CudaRandom pseudorandom seed is {0}" i
 
+    //inl a = 
+    //    open (Learning float32).Feedforward
+    //    player_pg {name="One"; actor=tanh 256; learning_rate=0.001f32} s
     inl a = 
-        open (Learning float32).Feedforward
-        player_pg {name="One"; actor=tanh 256; learning_rate=0.001f32} s
-    //inl a = player_zap_q {name="One"; steps_until_inverse_update=128; learning_rate=0.0001f32; discount_factor=0.99f32} s
+        inl learning_rate = {actor=0.001f32; critic=0.001f32; shared=0.001f32}
+        player_zap_ac {name="One"; learning_rate discount_factor=0.99f32; steps_until_inverse_update=128} s
     inl b = player_rules {name="Two"}
 
     met f game (!dyn near_to) (!dyn near_to_inner) = 
