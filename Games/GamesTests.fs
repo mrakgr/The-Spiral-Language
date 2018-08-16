e0f188ff40ced943f861d8f15147bb91bd2611d4
Marko Grdinić <mrakgr@gmail.com>
2018-08-16 10:41:16 +0200

    Finished the MC tests. MC it seems has a lot of variance, more than the AC, but not that much.
    Lame. None of the algorithms are good enough to use as they are. Let me bring in PRONG.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -148,7 +148,7 @@ f game 30 100000
     """
 
 let poker3 =
-    "poker3",[cuda_modules;loops;poker;poker_players;timer],"The iterative test for NN based players.",
+    "poker3",[cuda_modules;loops;poker;poker_players;timer],"The iterative test for NN TD(0) based players.",
     """
 inb s = CudaModules (1024*1024*1024)
 Struct.iter (inl use_steady_state ->
@@ -208,7 +208,58 @@ Struct.iter (inl use_steady_state ->
     ) (false, true)
     """
 
-output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) poker3
+let poker4 =
+    "poker4",[cuda_modules;loops;poker;poker_players;timer],"The iterative test for NN MC based players.",
+    """
+inb s = CudaModules (1024*1024*1024)
+Struct.iter (inl !dyn learning_rate ->
+    Console.printfn "The learning_rate is {0}" learning_rate
+    Loops.for {from=0; near_to=20; body=inl {i} ->
+        inl num_players = 2
+        inl stack_size = 10
+        inl max_stack_size = num_players * stack_size
+        open Poker {max_stack_size num_players}
+        open PokerPlayers {basic_methods State Action}
+
+        s.refresh
+        inb s = s.RegionMem.create'
+        s.CudaRandom.set_pseudorandom_seed (to uint64 i)
+
+        Console.writeline "------"
+        Console.printfn "The CudaRandom pseudorandom seed is {0}" i
+
+        inl a = 
+            open (Learning float32).Feedforward
+            player_pg {name="One"; actor=tanh 256; learning_rate} s
+        inl b = player_rules {name="Two"}
+
+        met f game (!dyn near_to) (!dyn near_to_inner) = 
+            Loops.for {from=0; near_to body=inl {i} ->
+                Timer.time_it (string_format "iteration {0}" i)
+                <| inl _ ->
+                    inl a = a.data_add {win=ref 0}
+                    inl b = b.data_add {win=ref 0}
+                    Loops.for {from=0; near_to=near_to_inner; body=inl {i} -> 
+                        s.refresh
+                        inb cd = s.RegionMem.create'
+                        inl a = a.data_add {cd}
+                        inb cd = s.RegionMem.create'
+                        inl b = b.data_add {cd}
+                        game stack_size (a, b)
+                        }
+                    inl a = a.data.win ()
+                    inl b = b.data.win ()
+                    Console.printfn "Winrate is {0} and {1} out of {2}." (a,b,a+b)
+                }
+
+        f game 15 1000
+        //open Poker {max_stack_size num_players log=Console.printfn}
+        //f game 10 1
+        }
+    ) (0.0003f32, 0.001f32, 0.003f32, 0.01f32)
+    """
+
+output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) poker4
 |> printfn "%s"
 |> ignore
 
