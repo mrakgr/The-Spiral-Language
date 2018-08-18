4823216b884bab48264fc4522574af53a9d687e7
Marko Grdinić <mrakgr@gmail.com>
2018-08-18 13:25:48 +0200

    ``` The learning_rate is 2 ** -10 ------ The CudaRandom pseudorandom seed is 0 Starting timing for: iteration 0 Winrate is 296 and 704 out of 1000. The time was 00:00:04.1227005 for: iteration 0 Starting timing for: iteration 1 Winrate is 460 and 540 out of 1000. The time was 00:00:05.7800941 for: iteration 1 Starting timing for: iteration 2 Winrate is 545 and 455 out of 1000. The time was 00:00:06.0888480 for: iteration 2 Starting timing for: iteration 3 Winrate is 609 and 391 out of 1000. The time was 00:00:05.8866857 for: iteration 3 Starting timing for: iteration 4 Winrate is 665 and 335 out of 1000. The time was 00:00:06.1007950 for: iteration 4 Starting timing for: iteration 5 Winrate is 673 and 327 out of 1000. The time was 00:00:05.8418674 for: iteration 5 Starting timing for: iteration 6 Winrate is 698 and 302 out of 1000. The time was 00:00:05.9385894 for: iteration 6 Starting timing for: iteration 7 Winrate is 744 and 256 out of 1000. The time was 00:00:05.8541158 for: iteration 7 Starting timing for: iteration 8 Winrate is 708 and 292 out of 1000. The time was 00:00:06.0134926 for: iteration 8 Starting timing for: iteration 9 Winrate is 715 and 285 out of 1000. The time was 00:00:06.3561601 for: iteration 9 Starting timing for: iteration 10 Winrate is 745 and 255 out of 1000. The time was 00:00:05.8427522 for: iteration 10 Starting timing for: iteration 11 Winrate is 734 and 266 out of 1000. The time was 00:00:05.9432436 for: iteration 11 Starting timing for: iteration 12 Winrate is 725 and 275 out of 1000. The time was 00:00:05.9353057 for: iteration 12 Starting timing for: iteration 13 Winrate is 728 and 272 out of 1000. The time was 00:00:06.2803105 for: iteration 13 Starting timing for: iteration 14 Winrate is 737 and 263 out of 1000. The time was 00:00:06.0072894 for: iteration 14 ```
    There does not seem to be a problem. Let me try increasing the learning rate to 2 ** -9.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -230,7 +230,7 @@ Struct.iter (inl i ->
                 open Feedforward
                 prong {learning_rate steps_until_inverse_update activation=Activation.tanh; size=256}
             //player_pg {name="One"; actor learning_rate} s
-            player_mc_ac {name="One"; actor; learning_rate; block_critic_gradients=false} s
+            player_mc_ac {name="One"; shared=actor; learning_rate; block_critic_gradients=false} s
         inl b = player_rules {name="Two"}
 
         met f game (!dyn near_to) (!dyn near_to_inner) = 
@@ -256,7 +256,7 @@ Struct.iter (inl i ->
         //open Poker {max_stack_size num_players log=Console.printfn}
         //f game 10 1
         }
-    ) (-10)
+    ) (-9)
     """
 
 output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) poker4
