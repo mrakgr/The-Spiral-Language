abc60cf251032452e27c1c636cb8d4f7b3633001
Marko Grdinić <mrakgr@gmail.com>
2018-11-26 17:23:27 +0100

    I'll admit, after programming for a full day, I am finally starting to get into it. The heat of struggle is starting to warm my soul. When I get through all of this and implement weight sampling it will ignite.
    Arnold Salas and Stefan Zohren. I'll be sure to remember their names for giving me a gift such as this.
    
    The Badam paper came out literally at the exact time that I most needed it. I'd like have missed weight sampling via the curvature on my own, but now that it has been pointed out to me I sure as hell will not miss it.
    
    All the prerequisites that I need to make a good player have been met.

---
+++ Learning/LearningTests.fs
@@ -0,0 +1,125 @@
+﻿module Learning.Tests
+
+open Spiral.Lib
+open Spiral.Tests
+open System.IO
+open Spiral.Types
+open Cuda.Lib
+
+let cfg = {Spiral.Types.cfg_default with trace_length=40; cuda_assert_enabled=false}
+
+let learning1 =
+    "learning1",[cuda_modules;learning;mnist;timer],"Does the training work with Mnist?",
+    """
+inb s = CudaModules (1024*1024*1024)
+
+inl float = float32
+open Learning float
+
+inl train_minibatch_size = 128
+inl test_minibatch_size = 128
+inl {test_images test_labels train_images train_labels} =
+    inl mnist_path = @"C:\ML Datasets\Mnist"
+    Mnist.load_mnist_tensors mnist_path
+    |> s.CudaTensor.from_host_tensors
+
+inl {train_images train_labels} = module_map (inl _ x -> x.round_split' train_minibatch_size) {train_images train_labels}
+inl {test_images test_labels} = module_map (inl _ x -> x.round_split' test_minibatch_size) {test_images test_labels}
+
+inl input_size = 784
+inl label_size = 10
+
+inl learning_rate = 2f32 ** -9f32
+inl network,_ =
+    open Feedforward
+    inl network =
+        relu 50,
+        linear label_size
+    //inl network =
+    //    prong {activation=Activation.relu; size=256},
+    //    prong {activation=Activation.linear; size=label_size}
+
+    init s input_size network
+
+inl train {data={input label} network learning_rate final} s =
+    inl near_to = fst input.dim
+    assert (near_to = fst label.dim) "The input and label must have the same outer dimension."
+    Loops.for' {from=0; near_to state=dyn 0.0; body=inl {i next state} ->
+        inl input, label = input i, label i
+        inl state =
+            inb s = s.RegionMem.create'
+            inl network, input = run s input network
+            inl {out bck} = final label input s
+
+            inl _ =
+                inl learning_rate = learning_rate ** 0.85f32
+                inl apply bck = bck {learning_rate} |> ignore
+                apply bck
+                Struct.foldr (inl {bck} _ -> Struct.foldr (inl bck _ -> apply bck) bck ()) network ()
+
+            Optimizer.standard learning_rate s network
+
+            inl cost = s.CudaTensor.get out |> to float64
+            state + cost
+
+        if nan_is state then state else next state
+        }
+    |> inl cost -> cost / to float64 input.span_outer2
+
+inl test {data={input label} network final} s =
+    inl near_to = fst input.dim
+    assert (near_to = fst label.dim) "The input and label must have the same outer dimension."
+    Loops.for' {from=0; near_to state=dyn {cost=0.0;ac=0;max_ac=0}; body=inl {i next state} ->
+        inl input, label = input i, label i
+        inl state =
+            inb s = s.RegionMem.create'
+            inl network, input = run s input network
+            inl {out} = final label input s
+
+            inl cost = out |> s.CudaTensor.get |> to float64
+            inl ac = Error.accuracy label input s |> s.CudaTensor.get
+            inl max_ac = (primal input).span_outer
+            {state with cost=self+cost; ac=self+ac; max_ac=self+max_ac}
+
+        if nan_is state.cost then state
+        else next state
+        }
+    |> inl cost -> {cost with cost = self / to float64 input.span_outer2}
+
+Loops.for' {from=0; near_to=5; body=inl {i next} -> 
+    inl final = Error.softmax_cross_entropy
+    inl cost =
+        //Timer.time_it (string_format "iteration {0}" i)
+        //<| inl _ ->
+            train {
+                data={input=train_images; label=train_labels}
+                network
+                learning_rate
+                final
+                } s
+
+    string_format "Training: {0}" cost |> Console.writeline
+
+    if nan_is cost then
+        Console.writeline "Training diverged. Aborting..."
+    else
+        inl {cost ac max_ac} =
+            test {
+                data={input=test_images; label=test_labels}
+                network
+                final
+                } s 
+
+        string_format "Testing: {0}({1}/{2})" (cost, ac, max_ac) |> Console.writeline
+        next ()
+    }
+
+Struct.iter (inl {weights={input}} ->
+    s.CudaTensor.print (primal input.data)
+    ) network
+    """
+
+output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) learning1
+|> printfn "%s"
+|> ignore
+
