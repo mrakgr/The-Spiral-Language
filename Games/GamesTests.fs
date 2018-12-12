964d6906fab9cc1c761d06728e54254ba67564d0
Marko Grdinić <mrakgr@gmail.com>
2018-12-12 10:48:40 +0100

    Let me try a bigger test now.

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -154,7 +154,7 @@ inb s = CudaModules (1024*1024*1024)
 Struct.iter (inl i ->
     inl learning_rate = 2f32 ** to float32 i |> dyn
     Console.printfn "The learning_rate is 2 ** {0}" i
-    Loops.for {from=0; near_to=1; body=inl {i} ->
+    Loops.for {from=0; near_to=10; body=inl {i} ->
         inl num_players = 2
         inl stack_size = 10
         inl max_stack_size = num_players * stack_size
@@ -200,7 +200,7 @@ Struct.iter (inl i ->
         //open Poker {max_stack_size num_players log=Console.printfn}
         //f game 10 1
         }
-    ) (-12)
+    ) (-13)
     """
 
 output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) poker3
