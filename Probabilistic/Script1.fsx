1bf8c3115d0db180037772c64cd9e6202eb3cfb7
Marko Grdinić <mrakgr@gmail.com>
2018-12-15 13:53:33 +0100

    What a waste of time that paper was. Let me just do some coding. Just for a bit.
    ```
    let rng = System.Random()
    
    let bernoulli p = if rng.NextDouble() >= p then 0.0 else 1.0
    let flip p = if rng.NextDouble() >= p then false else true
    let rec geometric p = if flip p then 1.0 + geometric p else 1.0
    
    let binomial _ =
        let a = bernoulli 0.5
        let b = bernoulli 0.5
        let c = bernoulli 0.5
        // TODO: Add factor and sample.
        a+b+c
    
    binomial()
    ```
    
    The glorious beginning starts with the first unremarkable step. At this point there is not much to do, but start by putting in the CPS monad. That will allow me to implement `sample` and `factor`. Those two operations are the crucial ones. They are what separate probabilistic languages from regular ones.
    
    It has been a while since I did any coding in F#. I have various versions of the CPS monad lying around somewhere, but I might as well do it from scratch. It won't be hard at all.

--- Probabilistic/Script1.fsx
+++ Probabilistic/Script1.fsx
@@ -1,1 +1,15 @@
-﻿//
+﻿let rng = System.Random()
+
+let bernoulli p = if rng.NextDouble() >= p then 0.0 else 1.0
+let flip p = if rng.NextDouble() >= p then false else true
+let rec geometric p = if flip p then 1.0 + geometric p else 1.0
+
+let binomial _ = 
+    let a = bernoulli 0.5
+    let b = bernoulli 0.5
+    let c = bernoulli 0.5
+    // TODO: Add factor and sample.
+    a+b+c
+
+binomial()
+
