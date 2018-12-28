041c81902d7c470a9b7715f22a2547db45a9b62c
Marko Grdinić <mrakgr@gmail.com>
2018-12-28 14:40:43 +0100

    To start things off, I do not think there will be a problem in moving the `Dictionary` module to the standard library.

--- The Spiral Language/SpiralLib.fs
+++ The Spiral Language/SpiralLib.fs
@@ -2626,3 +2626,39 @@ inl sign x =
 
 {pow sign} |> stackify
     """) |> module_
+
+let dictionary =
+    (
+    "Dictionary",[],"The Dictionary module.",
+    """
+stack inl {d with elem_type} ->
+    inl elem_type = type elem_type
+    inl key, value = elem_type
+    inl ty = fs [text: "System.Collections.Generic.Dictionary"; types: elem_type]
+    inl capacity = 
+        match d with
+        | {capacity} -> capacity : int32
+        | _ -> 64i32
+    inl id =
+        match d with
+        | {id} -> id
+        | _ -> .structural
+    inl x = 
+        match id with
+        | .structural -> macro.fs ty [type: ty; iter: "(",",",")", [arg: capacity; text: "HashIdentity.Structural"]]
+        | .reference -> macro.fs ty [type: ty; iter: "(",",",")", [arg: capacity; text: "HashIdentity.Reference"]]
+    function
+    | .count ->
+        macro.fs int32 [arg: x; text: ".Count"]
+    | .set i v ->
+        assert (eq_type key i) {msg="The index's type is not the equal to that of the key."; key i}
+        assert (eq_type value v) {msg="The second argument's type is not the equal to that of the value."; value v}
+        macro.fs () [arg: x; text: ".["; arg: i; text: "] <- "; arg: v]
+    | i {on_succ on_fail} ->
+        assert (eq_type key i) {msg="The index's type is not the equal to that of the key."; key i}
+        macro.fs () [arg: x; text: ".TryGetValue"; text: " <| "; arg: i; text: " |> fun (a,b) -> ";]
+        inl a = macro.fs bool [text: "a"]
+        inl b = macro.fs value [text: "b"]
+        if a then on_succ b else on_fail ()
+    |> stack
+    """) |> module_
