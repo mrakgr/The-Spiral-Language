b59919086ffd321659d4dab9748111351d37609b
Marko Grdinić <mrakgr@gmail.com>
2018-12-30 18:01:29 +0100

    "5:30pm.
    ```
            body=inl {state i next} ->
                inl action = player.act (observe state)
                transition action state <| function
                | .End -> player.optimize()
                | state ->
                    player.reward (reward state)
                    next state
    ```
    
    This is quite clean now.
    
    5:40pm. I am just thinking about what is next now.
    
    Game specific interfaces would be one.
    
    Well, let me just enjoy this moment for a bit and then I'll call it a day.
    
    I am rather please with how things turned out. I would not describe the kind of programming I did today as intense. It was rather tranquil actually. This kind of programming is perfectly acceptable. Piece by piece, I will get there. All I need to do is one more push and I will have succeeded in my goal of solving the Sea game with a tabular player.
    
    5:45pm. As the last thing for the day, let me put the player in its own module.
    
    6pm. Done. I put in some final touches.
    
    Unlike with the library refactoring, the testing for this won't be too bad. 15-30m at most.
    
    After I am done with the tabular player on the Sea game, I will move on to redoing the two learning tests.
    
    It is time to eat. I am done for the day."

--- Games/GamesTests.fs
+++ Games/GamesTests.fs
@@ -3,7 +3,7 @@
 open Spiral.Lib
 open Spiral.Tests
 open System.IO
-//open Module
+open Module
 open Cuda.Lib
 open Learning.Lib
 
@@ -127,95 +127,16 @@ inl Action = type .Left \/ .Right
 inl game player =
     Loops.for' {from=0; near_to=n; state={row=0; col=0}
         body=inl {state i next} ->
-            inl observation = observe state
-            inl action = player.act observation
+            inl action = player.act (observe state)
             transition action state <| function
-            | .End -> player.game_over
+            | .End -> player.optimize()
             | state -> 
-                inl reward = reward state
-                player.remember {observation action reward}
+                player.reward (reward state)
                 next state
         finally=ignore
         }
 
-inl player_tabular_sarsa {init elem_type=!(Tuple.wrap) elem_type} =
-    inl float = float32
-    inl zero = to float 0
-    inl dicts = 
-        Tuple.map (inl x ->
-            match x with
-            | {Observation Action} -> {x with dict = Dictionary {elem_type=int32, array float}}
-            | {Observation} -> {x with dict = Dictionary {elem_type=int32, float}}
-            ) elem_type
-    
-    inl rec act {learning_rate discount trace} obs =
-        inl rec loop = function
-            | {Observation=(_: obs) Action dict} :: _ ->
-                inl ar =
-                    inl i = Union.to_one_hot obs |> to int32
-                    dict i {
-                        on_fail = inl _ -> 
-                            inl ar = Array.init near_to (const init)
-                            dict.set i ar
-                            ar
-                        on_succ = id
-                        }
-
-                inl V, a =
-                    Loops.for {from=0; near_to state=dyn (-infinityf32, 0); body=inl {state=s,a i} ->
-                        inl V = ar i
-                        if V > s then V,i else s,a
-                        }
-
-                {
-                action=Union.from_one_hot Action a
-                bck=inl V' -> 
-                    ar a <- V + learning_rate * (V' - V)
-                    discount * ((trace - one) * V' + trace * V)
-                }
-            | {Observation=(_: obs) dict} :: _ ->
-                inl i = Union.to_one_hot obs |> to int32
-                inl V =
-                    dict i {
-                        on_fail = inl _ -> dict.set i init; init
-                        on_succ = id
-                        }
-
-                {
-                bck=inl V' -> 
-                    dist.set i (V + learning_rate * (V' - V))
-                    discount * ((trace - one) * V' + trace * V)
-                }
-            | _ :: next -> loop next
-            | () -> error_type {message = "The type of the argument matches none of the `elem_type`s."; argument=obs; elem_type}
-        loop dicts
-
-    inl reward rew = { bck = inl V' -> rew + V' }
-
-    {act reward}
-
-inl player_tabular_sarsa_interface {init elem_type learning_rate discount trace} =
-    inl float = float32
-    inl zero = to float 0
-    inl trace = ResizeArray.create {elem_type=float => float}
-
-    inl player = player_tabular_sarsa {init elem_type}
-
-    inl act obs =
-        inl {x with bck} = player.act {learning_rate discount trace} obs
-        trace.add (term_cast bck float)
-        match x with
-        | {action} -> action
-        | _ -> ()
-
-    inl reward x = 
-        inl {bck} = player.reward (to float x |> dyn)
-        trace.add (term_cast bck range)
-
-    inl optimize _ = trace.foldr (<|) zero |> ignore; trace.clear
-
-    heap {act reward optimize}
-
+inl player = PlayerTabular.create {init=1f32; elem_type={Observation Action}; learning_rate=0.01f32; trace=0.5f32; discount=1f32}
 ()
     """
 
