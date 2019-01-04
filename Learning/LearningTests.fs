547eceef1885dd84c4d70b9af01933ba44e1fb32
Marko Grdinić <mrakgr@gmail.com>
2019-01-04 19:10:10 +0100

    https://softwarefoundations.cis.upenn.edu/lf-current/Basics.html
    To be honest, I have no idea whether I could do this now. The simple ones obviously. I mean I did get up to chapter 5 last time (with some help), but unlike that Haskell fragment which was relatively straightforward, whether I am now capable of understanding this would need testing.
    
    https://softwarefoundations.cis.upenn.edu/lf-current/Induction.html
    
    The whole Coq tactics thing is as nasty as ever. But I think I would be open to challenging myself on this at some point.
    
    I'll leave it for after I am done with deep exploration.
    
    Let me rest for real now. My stress is skyrocketing due to this extra bit I am doing now.

--- Learning/LearningTests.fs
+++ Learning/LearningTests.fs
@@ -213,3 +213,4 @@ Loops.for' {from=0; near_to=5; body=inl {i next} ->
 output_test_to_temp cfg (Path.Combine(__SOURCE_DIRECTORY__, @"..\Temporary\output.fs")) learning2
 |> printfn "%s"
 |> ignore
+
