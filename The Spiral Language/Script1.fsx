a50b45c4f67559cd0af5236b600a9078c4fa7b39
Marko Grdinić <mrakgr@gmail.com>
2019-02-14 15:03:50 +0100

    "2:45pm. Maybe it has something to do with all those globals I am using now?
    v0.09 works, but v0.1 does not.
    
    2:50pm. https://stackoverflow.com/questions/42665926/cannot-resolve-dependency-to-assembly-fsharp-core-4-4-1-0-when-using-vs-2017/42666073
    
    Maybe I could try downloading F# Core of Nuget and see if that does anything?
    
    2:55pm.
    
    ```
    printfn "Hello"
    ```
    
    Even with a single file with just this, I still get the same error.
    
    Aggggghhhh, I spent 20m on this already. This is such bullshit.
    
    I just noticed that the project does not have a reference to `Fsharp.Core`.
    
    Well, let me take this chance to upgrade to 4.61.
    
    3pm. Ok, I finally manged to get through that retarded error.
    
    Let me finally run `test1` and get the real testing under way."

--- The Spiral Language/Script1.fsx
+++ The Spiral Language/Script1.fsx
@@ -1,40 +1,40 @@
-﻿#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsecCs.dll"
-#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsec.dll"
+﻿//#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsecCs.dll"
+//#r @"..\packages\FParsec.1.0.3\lib\net40-client\FParsec.dll"
 
-open FParsec
-open FParsec.CharParsers
+//open FParsec
+//open FParsec.CharParsers
 
-let skipChar x = skipChar x >>. spaces
-let var_name = many1SatisfyL (fun x -> isAsciiLetter x || isDigit x || x = '_' || x = ''') "variable" .>> spaces
-let let_ = skipString "let" >>. spaces >>. var_name .>> skipChar '=' .>> optional (skipString "//") .>> spaces .>> optional (skipChar '(')
-let name = between (skipChar '"') (skipChar '"') var_name .>> spaces
-let prerequisites = 
-    skipChar ',' >>. between (skipChar '[') (skipChar ']') (sepBy var_name (skipChar ';'))
-    |>> String.concat "; "
-let description = skipChar ',' >>. between (skipChar '"') (skipChar '"') (manySatisfy (fun x -> x <> '"'))
-let code = skipChar ',' >>. skipString "\"\"\"" >>. manyCharsTill anyChar (skipString "\"\"\"")
-let parser =
-    pipe5 let_ name prerequisites description code (
-        sprintf "let %s: SpiralModule =\n    {\n    name=\"%s\"\n    prerequisites=[%s]\n    description=\"%s\"\n    code=\n    \"\"\"%s\"\"\"\n    }\n"
-        )
-let parser' = parser .>> spaces .>> skipChar ')' .>> skipString "|>" .>> spaces .>> skipString "module_"
+//let skipChar x = skipChar x >>. spaces
+//let var_name = many1SatisfyL (fun x -> isAsciiLetter x || isDigit x || x = '_' || x = ''') "variable" .>> spaces
+//let let_ = skipString "let" >>. spaces >>. var_name .>> skipChar '=' .>> optional (skipString "//") .>> spaces .>> optional (skipChar '(')
+//let name = between (skipChar '"') (skipChar '"') var_name .>> spaces
+//let prerequisites = 
+//    skipChar ',' >>. between (skipChar '[') (skipChar ']') (sepBy var_name (skipChar ';'))
+//    |>> String.concat "; "
+//let description = skipChar ',' >>. between (skipChar '"') (skipChar '"') (manySatisfy (fun x -> x <> '"'))
+//let code = skipChar ',' >>. skipString "\"\"\"" >>. manyCharsTill anyChar (skipString "\"\"\"")
+//let parser =
+//    pipe5 let_ name prerequisites description code (
+//        sprintf "let %s: SpiralModule =\n    {\n    name=\"%s\"\n    prerequisites=[%s]\n    description=\"%s\"\n    code=\n    \"\"\"%s\"\"\"\n    }\n"
+//        )
+//let parser' = parser .>> spaces .>> skipChar ')' .>> skipString "|>" .>> spaces .>> skipString "module_"
 
-let qwe = 
-    """
-let test1 = 
-    "test1",[asd, wqe],"Does it run?",
-    ""
-inl a = 5
-inl b = 10
-a + b
-    ""
-    """
+//let qwe = 
+//    """
+//let test1 = 
+//    "test1",[asd, wqe],"Does it run?",
+//    ""
+//inl a = 5
+//inl b = 10
+//a + b
+//    ""
+//    """
 
-let file = @"C:\Users\Marko\Source\Repos\The Spiral Language\The Spiral Language\TextFile1.txt"
-let out = @"C:\Users\Marko\Source\Repos\The Spiral Language\The Spiral Language\TextFile1'.txt"
-match runParserOnFile (spaces >>. many (parser .>> spaces) .>> eof) () file System.Text.Encoding.Unicode with
-| Success(x,_,_) -> 
-    System.IO.File.WriteAllText(out,String.concat "\n" x)
-| Failure(x,_,_) ->
-    ()
+//let file = @"C:\Users\Marko\Source\Repos\The Spiral Language\The Spiral Language\TextFile1.txt"
+//let out = @"C:\Users\Marko\Source\Repos\The Spiral Language\The Spiral Language\TextFile1'.txt"
+//match runParserOnFile (spaces >>. many (parser .>> spaces) .>> eof) () file System.Text.Encoding.Unicode with
+//| Success(x,_,_) -> 
+//    System.IO.File.WriteAllText(out,String.concat "\n" x)
+//| Failure(x,_,_) ->
+//    ()
 
