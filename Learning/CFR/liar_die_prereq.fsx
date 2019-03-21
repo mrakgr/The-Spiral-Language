fc95b3200a965d3365381736fadf9fefaf00f82c
Marko Grdinić <mrakgr@gmail.com>
2019-03-21 14:58:41 +0100

    "2:20pm. https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
    Ah, damn this thing. I wish I'd been able to come up with it on my own. I had a sense there would be something simple like this. To think all I would need is a DFS with a add at the end.
    
    ```
    let outgoing_to_ingoing initial (graph: IDictionary<_,_>) =
        let nodes = Dictionary()
        let order = ResizeArray()
    
        let rec loop node: HashSet<_> =
            memoize nodes (fun node ->
                List.iter (loop >> fun x -> x.Add(node) |> ignore) graph.[node]
                order.Add(node)
                HashSet()
                ) node
    
        loop initial |> ignore
        nodes
        |> Seq.map (fun kv -> Seq.toList kv.Value, kv.Key)
        |> Seq.toList
        |> fun x -> order.ToArray() |> Array.rev, x
    
    outgoing_to_ingoing 1 (dict nodes_outgoing')
    ```
    
    So something like this?
    
    2:25pm. Yeah, it is like this. I was so close to the solution.
    
    2:50pm. I've just been deep in thought for the past 25m. Basically, I have 90% of what I need to make the memoizing CFR algorithm. The rest I will work out while actually doing the implementation.
    
    Rather than start with Liar Die, I'd be better off doing the one action Dudo, so I can get the design down without getting bogged with chance nodes and other useless implementation details.
    
    It is really coming down beautifully in my mind.
    
    To be honest, I am not even looking at the crap tier code in the paper. Once I know the idea behind it, it is actually easier for me to come up with my own version.
    
    ...But I suppose I should study it for a bit. Let me take a break here first, as I need to do the chores as is usual at this time."

--- Learning/CFR/liar_die_prereq.fsx
+++ Learning/CFR/liar_die_prereq.fsx
@@ -11,18 +11,23 @@ let nodes_outgoing, _ = List.mapFoldBack (fun a s -> (a,s), a :: s) nodes []
 /// Shuffled
 let nodes_outgoing' = [(1, [3; 4; 2]); (3, [4]); (4, []); (2, [4; 3])]
 
+/// Returns the topological order of the graph. Converts outgoing to ingoing edges.
+/// Lacks checking for recursive cycles.
 let outgoing_to_ingoing initial (graph: IDictionary<_,_>) =
     let nodes = Dictionary()
+    let order = ResizeArray()
 
     let rec loop node: HashSet<_> = 
         memoize nodes (fun node -> 
             List.iter (loop >> fun x -> x.Add(node) |> ignore) graph.[node]
+            order.Add(node)
             HashSet()
             ) node
 
     loop initial |> ignore
     nodes
-    |> Seq.map (fun kv -> Seq.toList kv.Value, kv.Key)
+    |> Seq.map (fun kv -> kv.Key, Seq.toArray kv.Value)
     |> Seq.toList
+    |> fun x -> order.ToArray() |> Array.rev, dict x
 
 outgoing_to_ingoing 1 (dict nodes_outgoing')
