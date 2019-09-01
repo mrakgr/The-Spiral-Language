71916de6fb9a6607cc74e7924499d81ad0e33b58
Marko Grdinić <mrakgr@gmail.com>
2019-09-01 10:26:44 +0200

    https://www.reddit.com/r/ProgrammingLanguages/comments/cy009u/patterns_as_types/ "[Spiral](https://github.com/mrakgr/The-Spiral-Language/tree/v0.1) actually has this as a consequence of doing typechecking via partial evaluation. Here is an example:
    ```
    let test8': SpiralModule =
        {
        name="test8"
        prerequisites=[console]
        opens=[]
        description="Does the basic union type work?"
        code=
        """
    inl big_type = (A: 0) \/ (B: 0, 0) \/ .C // The \/ operator converts `0i64` literals to `type int64`
    print_static big_type // type (union (A:(int64) | B:((int64, int64)) | C))
    inl small_type = (A: 0) \/ .C
    print_static small_type // type (union (A:(int64) | C))
    inl f = function
        | #(A: x) -> x
        | #(B: a,b) -> a+b
        | #(.C) -> 0
    
    Console.writeline <| f (dyn (Type box: (A: 3) to: small_type)) // prints 3
    Console.writeline <| f (dyn (Type box: (A: 5) to: big_type)) // prints 5
        """
        }
    ```
    
    Here is the compiled F# code.
    
    ```
    type SpiralType0 =
        | SpiralType0_1 of int64
        | SpiralType0_2
    and SpiralType3 =
        | SpiralType3_1 of int64
        | SpiralType3_4 of int64 * int64
        | SpiralType3_2
    let ((var_1 : SpiralType0)) = SpiralType0_1 3L
    let ((var_3 : int64)) =
        match var_1 with
        | SpiralType0_1 var_2 ->
            var_2
        | SpiralType0_2 ->
            0L
    let () = System.Console.WriteLine(var_3)
    let ((var_4 : SpiralType3)) = SpiralType3_1 5L
    let ((var_9 : int64)) =
        match var_4 with
        | SpiralType3_1 var_5 ->
            var_5
        | SpiralType3_4 (var_6, var_7) ->
            var_6 + var_7
        | SpiralType3_2 ->
            0L
    let () = System.Console.WriteLine(var_9)
    ```
    
    You can erase the `| #(B: a,b) -> a+b` case in which case you'd get a type error on the last line.
    
    ```
    Error trace on line: 11, column: 19 in module test8.
    Console.writeline <| f (dyn (Type box: (A: 5) to: big_type)) // prints 5
                      ^
    Error trace on line: 7, column: 7 in module test8.
        | #(A: x) -> x
          ^
    Error trace on line: 8, column: 7 in module test8.
        | #(.C) -> 0
          ^
    Pattern miss error. The argument is var (union (A:(int64) | B:((int64, int64)) | C))
    ```
    
    In my opinion, you'd need to typechecking like Spiral does to get its brand of pattern matching. Spiral enables the kind of reasoning about programs that is quite similar to standard functional programming, yet quite distinct in its character.
    
    I wish I had an algorithm worth implementing in it. It has been quite a while since I last wrote Spiral code."
    
    Made a little post in that thread. I guess this will serve as a warm up for the day. Let me chill just a bit and then I will start. I want to get a RA lecture out of the way.

--- Testing/run.fs
+++ Testing/run.fs
@@ -1802,7 +1802,7 @@ let euler3: SpiralModule =
     description="Largest palindrome product"
     code=
     """
-//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 ï¿½ 99.
 //Find the largest palindrome made from the product of two 3-digit numbers.
 
 inl reverse_number x =
