0f0ca4181c8aaba823da0eef20f60f0be702eee8
Marko Grdinić <mrakgr@gmail.com>
2019-09-27 17:56:21 +0200

    I'll finish this up tomorrow. There are some ambiguities that I need to work through first.

--- Learning/CFR/Monte Carlo/Outcome sampling/CFR.fsx
+++ Learning/CFR/Monte Carlo/Outcome sampling/CFR.fsx
@@ -5,57 +5,42 @@ open Helpers
 open System.Collections.Generic
 
 type Node = 
-    {
-    strategy_sum: float []
-    regret_sum: float []
-    mutable count: uint32
-    }
+        {
+        strategy_sum: float []
+        regret_sum: float []
+        mutable count: uint32
+        }
 
 let node_create actions = 
     let l = Array.length actions
     {strategy_sum=Array.zeroCreate l; regret_sum=Array.zeroCreate l; count=0u}
 
-type Particle<'state, 'history> = 
-    {
-    state: 'state
-    probability: Probability
-    uniformity_coef: float
-    count: uint32
-
-    infosets: Dictionary<'history, Node>
-    is_updateable: bool
-    }
-
-//let particle state = {state=state; propositional_probability=1.0; sampling_probability=1.0}
-
-let state x = x.state
-let inline chance dice one next = 
-    let dice = sample_uniform dice 
+let inline chance dice next = 
     /// Outcome sampling does not consider the probabilities in chance nodes
-    next {one with state=dice}
+    next (sample_uniform dice)
 
-let inline response_first node one two actions next = 
-    let node = memoize one.infosets (fun _ -> node_create actions) history
+let inline response_regret infosets uniformity_coef history actions one_probability two_probability next =
+    let node = memoize infosets (fun _ -> node_create actions) history
     let proposal_distribution = regret_match node.regret_sum
-    let sampling_distribution = Array.map (fun prob -> one.uniformity_coef * (1.0 / float (Array.length actions)) + (1.0 - one.uniformity_coef) * prob) proposal_distribution
-    let action, node_probability, i' = sample actions sampling_distribution proposal_distribution
-    let utility, tail_probability = next (action, {one with probability=one.probability * node_probability})
-    let W = utility * two.probability.proposal // for some reason importance sampling is not being done here
+    let sampling_distribution = Array.map (fun prob -> uniformity_coef * (1.0 / float (Array.length actions)) + (1.0 - uniformity_coef) * prob) proposal_distribution
+    let action, node_probability, action_index = sample actions sampling_distribution proposal_distribution
+    let utility, tail_probability = next action (one_probability * node_probability)
+    let W = utility * two_probability.proposal // For some reason the importance sampling correction is not being done here.
     for i=0 to actions.Length-1 do
-        let r =
-            if i = i' then -W * node_probability.proposal
-            else W * (one.probability.proposal * (proposal_distribution.[i] - 1.0))
+        // The branches seem to be the other way around between the thesis pseudocode and Lanctot's C++ examples.
+        // Also, what `pi(z[i]a,z)` means in the context is ambiguous.
+        let r = if i = action_index then W * (1.0 - proposal_distribution.[i]) else -W
         node.regret_sum.[i] <- node.regret_sum.[i] + r
     -utility, tail_probability * node_probability.proposal
 
-let inline respose_second node one two actions next =
-    let node = memoize one.infosets (fun _ -> node_create actions) history
+let inline response_strategy infosets count history actions one_probability next =
+    let node = memoize infosets (fun _ -> node_create actions) history
     let proposal_distribution = regret_match node.regret_sum
     let action, node_probability, _ = sample_uniform' actions proposal_distribution
-    let utility, tail_probability = next (action, {one with probability=one.probability * node_probability})
+    let utility, tail_probability = next action (one_probability * node_probability)
     for i=0 to actions.Length-1 do
-        node.strategy_sum.[i] <- node.strategy_sum.[i] + float (one.count - node.count) * two.probability.proposal * node_probability.proposal
-        node.count <- one.count
+        node.strategy_sum.[i] <- node.strategy_sum.[i] + float (count - node.count) * one_probability.proposal * node_probability.proposal
+        node.count <- count
     -utility, tail_probability * node_probability.proposal
 
-let terminal one two x = x * one.probability.sample * two.probability.sample, 1.0
+let terminal one_probability two_probability x = x * one_probability.sample * two_probability.sample
