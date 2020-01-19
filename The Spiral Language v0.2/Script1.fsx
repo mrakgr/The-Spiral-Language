abc66c42c3243da070b9218322ebec9fcb33cb55
Marko Grdinić <mrakgr@gmail.com>
2020-01-19 13:31:12 +0100

    "10:45am. Uf, what a late awakening, but who cares about that. I am enjoying myself even though I really indulged myself way too much last night and had trouble sleeping as a result.
    Yesterday I left the last two chapters of vol 10 for today, so I am going to take care of that now.
    
    11am. I guess I will extend the vacation to today.
    
    Let me just say that the last few weeks of downtime really charged me up. I am almost ready to fully dedicate myself to programming.
    
    1:10pm. I am done with breakfast. Chores are still left, but since I intend to make this the last day of vacation, I am not going to push myself. Let me watch some anime, then do the chores and after that I will start.
    
    This is my reward for all my hard work over the past 5 years.
    
    When I resume I am going to do this for real.
    
    I am going to give myself to my inspiration, and this time reach my goal without exception. Ultimately, my obsession with fixing backprop will be just a drop in the pond once I bring out the ideas that I have.
    
    The move that I will make will be absolutely perfect.
    
    While solving the backprop bottleneck and figuring out how to do long term credit assignment is important, instead of charging that mill I can use a strategy that will ensure that I get good benefit now and great benefits later once the breakthrough comes at the hands of somebody else.
    
    For all I know, this might be the right direction to go in to find that breakthrough on my own.
    
    Basic capabilities really do matter greatly in this race, and with just one more step I can shake off my beginner's affects.
    
    1:20pm. I cannot describe it. All these rants are one thing, but I've been feeling for a few weeks that I can really tap into the power of evolution to get me what I want. As a greedy improvement algorithm, backprop will do just fine. On it own, its flaws are magnified, but I think I've been too hard on it. Reality is a harsh place. It is not its fault it cannot do hierarchies without a few tricks.
    
    Just like I have dropped type theory and dependent types from the list of desirable PL features, I will get rid of all higher order optimizers in the domain of ML that I will do. I will continue cutting as such until I reach the apex of simplicity.
    
    Worse is okay, and good is bad.
    
    The gradient as I can see is really pointing me strongly in this direction.
    
    All these very difficult exercises might not have taught me how to crack the puzzle itself, but now I can see that certain things which on the surface should be obvious are actually very hard. I do not need to beat myself up for not being able to deal with them directly."

--- The Spiral Language v0.2/Script1.fsx
+++ The Spiral Language v0.2/Script1.fsx
@@ -1,13 +1,1 @@
-﻿open System.Collections.Generic
-
-type [<ReferenceEquality>] A = A of obj
-type B = B of int * A
-
-let x = A "qwe"
-let y = A "qwe"
-B(1,x) = B(2,x)
-
-let Q : HashSet<B> = HashSet(HashIdentity.Structural)
-Q.Add(B(2,x))
-
-hash (B(2,x))
+﻿
