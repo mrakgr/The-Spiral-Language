841fa46bbe2b6aea8e92219396aae95f0b495f2a
Marko Grdinić <mrakgr@gmail.com>
2020-04-13 11:34:06 +0200

    "8:35am. I am groggy as hell, but finally I am up early for once. Let me do my usual morning reading and I'll start once I dust off the cobwebs.
    9am. Ok, I am recovering slowly. Let me read Hellrage and then I'll start.
    
    9:15am. Let me start.
    
    Time to finish that chapter.
    
    9:35am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/elm/project-structure
    
    I just skimmed the previous chapter. I do not really have to understand how Elmish works in depth right now or how to use it. What I am really interested in is in fact this. How do you get Fable to compiler switch between compiling to the browser and Node. If I can figure that out I'll also know to how to target VS Code extensions.
    
    Let me stop here for a bit though first. I need a short break.
    
    10:15am.
    
    ```
            let delayedDispatch = async {
                let! msg = operation
                dispatch msg
            }
    
            Async.StartImmediate delayedDispatch
    ```
    
    Ok, let me stop for a moment here. Does this really launch tasks asynchronously. What the hell? I need to figure it out.
    
    ```
    open System
    
    async {
        printfn "Start 1."
        do Threading.Thread.Sleep(1000)
        printfn "Done 1."
    } |> Async.Start
    
    async {
        printfn "Start 2."
        do Threading.Thread.Sleep(1000)
        printfn "Done 2."
    } |> Async.Start
    ```
    
    The way to run an `async` operation asynchronously is `Async.Start`. `Async.StartImmediate` does it synchronously as I well know. Why does the JS code get async behavior in that case? Bizarre.
    
    ...Maybe `Async.sleep` is run asynchronously even when the rest is sync?
    
    ```
    async {
        printfn "Start 1."
        do! Async.Sleep(1000)
        printfn "Done 1."
    } |> Async.StartImmediate
    
    async {
        printfn "Start 2."
        do! Async.Sleep(1000)
        printfn "Done 2."
    } |> Async.StartImmediate
    ```
    
    ```
    Start 1.
    Start 2.
    val it : unit = ()
    
    > Done 1.
    Done 2.
    ```
    
    Motherfucker. That really was it. You got me good.
    
    It just goes to show that I do not really understand `Async` that well. Not as well as I thought I did anyway. I've been thinking of getting that .NET concurrency book, but I'll leave that for some other time.
    
    I am happy at only being able to use it well right now.
    
    10:30am.
    
    ```
    type Msg =
      // messages for Operation 1 -> might fail
      | StartOperationOne
      | FinishOperationOneSuccessfully of ResultOfOne
      | FinishOperationOneWithError of exn
    ```
    
    This is the same form as observables.
    
    10:40am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/commands/recursive-updates
    
    I am really skimming through this. I'll be aiming to go through the entire part 3 before breakfast. I do not want to spend too much time on this. A single day should be enough to get through the entire book at this rate.
    
    11:05am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/commands/xhr-elmish
    
    Enough, let me just skip to the next chapter. I am not even reading things at this point.
    
    11:10am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/dev-flow/
    
    Let me just move to the last chapter. The rest is something I would be interested in if I were doing webdev with Fable, but I just want to do compiler plugins.
    
    Let me go through this chapter and then I'll look into setting up a project.
    
    11:20am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/dev-flow/webpack-mode
    
    I really like how ReasonML will clean up the code and produce readable JS. So far Fable was not good in that regard.
    
    11:30am. https://zaid-ajaj.github.io/the-elmish-book/#/chapters/dev-flow/compiler-directives
    
    The tedium of this is killing me. Let me stop here so I can have breakfast. I'll deal with this properly today. Tonight I slept well, but I got up too early and now I am yawning here."

--- The Spiral Language v0.2/Script1.fsx
+++ The Spiral Language v0.2/Script1.fsx
@@ -1,1 +1,13 @@
-﻿
+﻿open System
+
+async {
+    printfn "Start 1."
+    do! Async.Sleep(1000)
+    printfn "Done 1."
+} |> Async.StartImmediate
+
+async {
+    printfn "Start 2."
+    do! Async.Sleep(1000)
+    printfn "Done 2."
+} |> Async.StartImmediate
