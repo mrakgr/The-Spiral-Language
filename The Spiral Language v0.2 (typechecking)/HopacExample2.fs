2d6dec302835b368c8d263bbe41d9d37e99cd26c
Marko Grdinić <mrakgr@gmail.com>
2020-06-12 08:38:13 +0200

    "8:10am. I hope nobody answered the SO question from yesterday, because I've figured it out. The reason why you need two channels is to force the users to go through the server. You'd be able to tell what the problem with using just a single channel is if you removed the server entirely.
    ...Nobody answered it, great. Let me try my hypothesis.
    
    8:25am. https://stackoverflow.com/questions/62327130/why-are-two-bidirectional-channels-needed-to-implement-the-server-protocol-for-a/62338633#62338633
    
    I did it. Great.
    
    ```fs
    let cell x =
        let c = Ch ()
        Job.start (put c x) >>-. c
    ```
    
    I wonder why this `Job.start` is needed here?
    
    ...Is it because it blocks the main thread otherwise? That could be it.
    
    ```
    module HopacExample2
    
    open System
    open Hopac
    open Hopac.Infixes
    
    let get c = Ch.take c
    let put c (x: 'a) = Ch.give c x
    
    run <| job {
        let c = Ch ()
        let print () = Job.start (get c >>- fun i -> printf "%i\n" i)
        let put i = Job.start (put c i)
        for i=1 to 6 do
            do! print()
            do! put i
        }
    
    Console.ReadKey()
    ```
    
    That has to be it. Yeah, I get it.
    
    8:35am. I am quite pleased that I managed to figure it out on my own. The answer came to me in the morning as soon as I woke up. That this is happening gives me hope.
    
    Let me do my usual morning reading and then I'll start after that. The plan is to both study Reppy's book and prototype the language server side by side. To start things off, I think I will study even though I really should be prototyping."

--- The Spiral Language v0.2 (typechecking)/HopacExample2.fs
+++ The Spiral Language v0.2 (typechecking)/HopacExample2.fs
@@ -4,14 +4,16 @@ open System
 open Hopac
 open Hopac.Infixes
 
-let hello what = job {
-  for i=1 to 3 do
-    do! timeOut (TimeSpan.FromSeconds 1.0)
-    do printfn "%s" what
-}
+let get c = Ch.take c
+let put c (x: 'a) = Ch.give c x
 
-[
-timeOut (TimeSpan.FromSeconds 0.0) >>=. hello "Hello, from first job!"
-timeOut (TimeSpan.FromSeconds 0.3) >>=. hello "Hello, from second job!"
-timeOut (TimeSpan.FromSeconds 0.6) >>=. hello "Hello, from third job"
-] |> Job.conIgnore |> run
+run <| job {
+    let c = Ch ()
+    let print () = Job.start (get c >>- fun i -> printf "%i\n" i)
+    let put i = Job.start (put c i)
+    for i=1 to 6 do
+        do! print()
+        do! put i
+    }
+
+Console.ReadKey()
