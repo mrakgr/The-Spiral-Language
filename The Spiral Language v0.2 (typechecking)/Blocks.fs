3e19bb260e76bddf97a316df25b6ebaaa80df587
Marko Grdinić <mrakgr@gmail.com>
2020-06-25 11:16:56 +0200

    "9:40am. Let me chill a bit before I start.
    9:50am. I've had a bunch of time to think about how I want to do this. I should just stick to the usual system - make the whole thing as close to the naive version and then do memoization in select places. That is the way to go.
    
    10:10am. Ok, focus me. Enough of the Blattodea thread. Let me just read the latest chapter of Hellrage and then I will do some programming.
    
    10:35am. Let me start. It is time to get the ball rolling.
    
    To start with, I've decided on something during the night. Rather than transfering the range, it would be better if I transfer the specific lines. That way there is no need to do the splitting. It would negligibly slow down the transfer.
    
    Mostly, I want this because it would be a pain to change the lines directly.
    
    10:40am. I am distracted. Focus me, focus. It is always a hard slog.
    
    Before I start this part seriously, I'll try prototyping it. Maybe that will give me some feeling for how to do blocks. Oh, yeah. It might not be good to assume that blocks during earlier stages and during typechecking are the same thing.
    
    ```fs
    module Blocks
    
    type Lang =
        | Separator
        | Line of string
    ```
    
    How about I do something like this?
    
    ```fs
    type File = Lang []
    ```
    
    Hmmm, better make it an resizable array.
    
    10:50am.
    
    ```fs
    type Lang =
        | Separator
        | Line of string
    type File = Lang ResizeArray
    
    type Block = string []
    type FileBlocks = Block ResizeArray
    ```
    
    Let me start with this. The real thing will be even harder as I have those tricky comments to deal with, but I should be able to understand if I can make the system work with this.
    
    Most of the things I need here, I already did in the prototype - the file parsing parts, but doing the mapping here is more involved.
    
    10:55am. The first thing I need to do is to implement the remove and insert functions. For starters, I need to decide wther I want to keep around a `type File = Lang ResizeArray` or to just operator on `FileBlocks`.
    
    ...I have no idea how to handle this elegantly at all. All the operations that I need are not hard in isolation, but this problem feels far harder than it should be. I would not be surprised if I spent a few days playing with it.
    
    11am.
    
    ```fs
    let insert (blocks : FileBlocks) (pos : int) (lines : Lang []) =
        ()
    ```
    
    Let me try implementing this.
    
    11:05am.
    
    ```fs
    let insert (blocks : FileBlocks) (pos : int) (lines : Lang []) =
        let rec find c i =
            if i < blocks.Count then
                let c = c + blocks.[i].Length
                if pos <= c then
                else find c (i+1)
            else failwith "Can't insert out of bounds."
    ```
    
    This is not too hard, but I feel a great sense of inertia as I write this that I have to force myself to overcome. When I go through this, boy will it feel good.
    
    First, let me convert the lines to blocks.
    
    Maybe rather than inserting lines into blocks, it would be better to try inserting blocks into blocks.
    
    One thing I am confused is how to handle `blocks` being empty. I keep imagining it from a filled perspective.
    
    Yeah, I can simplify things even more. Since a single block is just an array of lines, let me do something.
    
    11:15am.
    
    ```fs
    let insert_block (block : Block) pos (x : Block []) =
        assert (pos <= block.Length)
    ```
    
    This is promising.
    
    Now let me split the block at `pos`.
    
    ...Let me do the chores here. I'll be back in a jiffy."

---
+++ The Spiral Language v0.2 (typechecking)/Blocks.fs
@@ -0,0 +1,22 @@
+﻿module Blocks
+
+type Lang =
+    | Separator
+    | Line of string
+type File = Lang ResizeArray
+
+type Block = string []
+type FileBlocks = Block ResizeArray
+
+let insert_block (block : Block) pos (x : Block []) =
+    assert (pos <= block.Length)
+    let l,r = Array.splitAt pos block
+
+let insert (blocks : FileBlocks) (pos : int) (lines : Lang []) =
+    let rec find c i =
+        if i < blocks.Count then
+            let c' = c + blocks.[i].Length
+            if pos <= c' then failwith ""
+            else find c' (i+1)
+        else failwith "Can't insert out of bounds."
+    ()
