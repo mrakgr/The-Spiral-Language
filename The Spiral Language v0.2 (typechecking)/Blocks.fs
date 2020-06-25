3e9eea864faafafa8cea2fd23e2908689646ec55
Marko Grdinić <mrakgr@gmail.com>
2020-06-25 12:23:35 +0200

    "11:30am. I am back. Time to resume.
    ```fs
    let insert_block (block : Block) pos (x : Block []) =
        assert (pos <= block.Length)
        let l,r = Array.splitAt pos block
    ```
    
    Before I left I did this. Now all I need to do is merge this.
    
    ```
    Array.concat [[|l|];x;[|r|]]
    ```
    
    No, this is not quite right. I want to merge the left with the first block of the array, and the right with the last.
    
    11:45am.
    
    ```fs
    let insert_block (block : Block) pos (x : Block []) =
        if Array.isEmpty x then [|block|] else
        let l,r = Array.splitAt pos block
        let x = Array.copy x
        x.[0] <- Array.append l x.[0]
        let last = x.Length-1
        x.[last] <- Array.append x.[last] r
        x
    ```
    
    Yeah, this is wonderful. This is exactly what the insertion functionality should be. Now that I have this, it will make thinking about the rest easier.
    
    ...Oh, yes. The next thing on the list is to convert an array of lines to an array of blocks.
    
    This should be easy.
    
    ```fs
    type Line =
        | Separator
        | Text of string
    type File = Line ResizeArray
    ```
    
    Let me name it better.
    
    11:55am.
    
    ```fs
    let lines_to_blocks (lines : Line []) : Block [] =
        let blocks = ResizeArray()
        let block = ResizeArray()
        let blocks_add () = blocks.Add(block.ToArray()); block.Clear()
        lines |> Array.iter (function Separator -> blocks_add() | Text t -> block.Add(t))
        blocks_add()
        blocks.ToArray()
    ```
    
    This should be good.
    
    Now let me do `insert`.
    
    12:05pm.
    
    ```fs
    type Line = Separator | Text of string
    type Block = string []
    
    let lines_to_blocks (lines : Line []) : Block [] =
        let blocks = ResizeArray()
        let block = ResizeArray()
        let blocks_add () = blocks.Add(block.ToArray()); block.Clear()
        lines |> Array.iter (function Separator -> blocks_add() | Text t -> block.Add(t))
        blocks_add()
        blocks.ToArray()
    
    let insert_block (block : Block) pos (x : Block []) =
        if Array.isEmpty x then [|block|] else
        let l,r = Array.splitAt pos block
        let x = Array.copy x
        x.[0] <- Array.append l x.[0]
        let last = x.Length-1
        x.[last] <- Array.append x.[last] r
        x
    
    let insert (blocks : Block ResizeArray) (pos : int) (lines : Line []) =
        let rec loop pos i =
            let l = blocks.[i].Length
            if pos <= l then
                blocks.RemoveAt(i)
                blocks.AddRange(insert_block blocks.[i] pos (lines_to_blocks lines))
            else loop (pos-l) (i+1)
        loop pos 0
    ```
    
    I think this is a pretty decent approximation of how block insertion should be done.
    
    12:10pm. In the real thing, I will have difficulty with comments since they will cause the block separation to be context dependent...
    
    You know what, mutually recursive blocks and comments are too much of a pain in the ass. I will just have the comments be separators period. The same goes for `and`. The later stage can merge them into a single block.
    
    Let me figure out how to tackle the problem at hand first.
    
    So here I have insertion done.
    
    The next thing needs to be the opposite of that - removal.
    
    12:20pm. Let me take a break here.
    
    Agh...there is just no easy way to get through this. I'll do it one step at a time."

--- The Spiral Language v0.2 (typechecking)/Blocks.fs
+++ The Spiral Language v0.2 (typechecking)/Blocks.fs
@@ -1,22 +1,30 @@
 ﻿module Blocks
 
-type Lang =
-    | Separator
-    | Line of string
-type File = Lang ResizeArray
-
+type Line = Separator | Text of string
 type Block = string []
-type FileBlocks = Block ResizeArray
+
+let lines_to_blocks (lines : Line []) : Block [] =
+    let blocks = ResizeArray()
+    let block = ResizeArray()
+    let blocks_add () = blocks.Add(block.ToArray()); block.Clear()
+    lines |> Array.iter (function Separator -> blocks_add() | Text t -> block.Add(t))
+    blocks_add()
+    blocks.ToArray()
 
 let insert_block (block : Block) pos (x : Block []) =
-    assert (pos <= block.Length)
+    if Array.isEmpty x then [|block|] else
     let l,r = Array.splitAt pos block
+    let x = Array.copy x
+    x.[0] <- Array.append l x.[0]
+    let last = x.Length-1
+    x.[last] <- Array.append x.[last] r
+    x
 
-let insert (blocks : FileBlocks) (pos : int) (lines : Lang []) =
-    let rec find c i =
-        if i < blocks.Count then
-            let c' = c + blocks.[i].Length
-            if pos <= c' then failwith ""
-            else find c' (i+1)
-        else failwith "Can't insert out of bounds."
-    ()
+let insert (blocks : Block ResizeArray) (pos : int) (lines : Line []) =
+    let rec loop pos i =
+        let l = blocks.[i].Length
+        if pos <= l then
+            blocks.RemoveAt(i)
+            blocks.AddRange(insert_block blocks.[i] pos (lines_to_blocks lines))
+        else loop (pos-l) (i+1)
+    loop pos 0
