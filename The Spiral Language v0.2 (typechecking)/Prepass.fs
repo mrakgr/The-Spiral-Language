9edb4d6b1bdf8c288313852ed8a4ac8258bec129
Marko Grdinić <mrakgr@gmail.com>
2020-09-12 12:17:13 +0200

    "9:50am. Too much fun with Reverend Insanity again.
    Let me chill for a while and then I will start. I've made up my mind to just focus on doing the filling.
    
    10:20am. Ok, just a bit more and I will start. Since I got up this late, and am even feeling hungry I do feel like skipping the morning sesssion, but this time specifically I won't skip. I want to build up some momentum. I want to at least break the ice in the morning.
    
    10:25am. Let me start.
    
    I should get something done. I should start the first part of the prepass.
    
    10:30am.
    
    ```fs
    and TExpr =
        | B of Range
        | Var of Range * Id
        | Pair of Range * TExpr * TExpr
        | Arrow of Range * TExpr * TExpr
        | Record of Range * Map<string,TExpr>
        | Symbol of Range * string
        | Apply of Range * TExpr * TExpr
        | Prim of Range * Config.PrimitiveType
        | Term of Range * Expr
        | Macro of Range * Macro list
        | Fun of Range * FreeVars * Id * TExpr
    ```
    
    I am confused. Shouldn't I have something for higher order types here?
    
    ```fs
    | HigherOrder of Range * Id
    ```
    
    Let me add this.
    
    10:35am.
    
    ```fs
        | Fun of Range * FreeVars * Id * Expr
        | Recursive of (Range * FreeVars * Id * Expr) ref
    ```
    
    Let me do this one like so.
    
    ```fs
    type TopEnv = {
    
        }
    ```
    
    The most important thing I need to do before I can start is define `TopEnv`.
    
    10:40am. It is strange that this is giving me trouble. I think I understand the later steps well enough...
    
    11:05am. I am thinking. I really meant to start, but it appears I need to refine the initial part.
    
    Damn it. Last time I only had to prepass the inl statements and that was straightfoward. Now that I need to do the top level, along with everything else, the prepass will take a lot more work. Yeah, I won't be able to do this that easily. Still, it should not take anywhere near as long as the typechecker.
    
    11:35am. Had to take a break. I am still thinking about it full throttle.
    
    ```fs
    //type FreeVars = {|type' : int; term : int|}
    type FreeVars = unit
    ```
    
    Let me do this. I've decided that I'll first make the skeleton and then the muscles. Prepass used to be all about propagating the free variable information, but for the time being I'll just leave this as unit.
    
    11:40am. That will make the bite easier to swallow.
    
    Free variables are not such a huge problem. I can leave them for later.
    
    Before I should do the general form of it. This stuff will be new to the prepass.
    
    11:45am.
    
    ```fs
    | Apply of Range * Expr * Expr
    ```
    
    Let me get rid of this. I do not need it after all.
    
    12pm. Damn it. Even though I've made the decision to ignore free vars for the time being, my mind is still in too much disarray to start. Let me have breakfast and then maybe I'll spend the rest of the day in bed. Whether it is my loneliness or insecurity, they seem to be at full blast right now. I am just too weak.
    
    12:05pm. It is tough. It is always tough. I need to take my weakness and my strength and convert it to inspiration. If I had the Inspired Desire that process would be seamless and without friction. Since I am just a human, I need to dispense with the pretense and just make do with what I have.
    
    As pathetic as it is, I am going to have to retreat until I feel the time is ripe to attack.
    
    This brainstorming is just the worst. It is the most difficult part of the whole thing, mentally speaking. To make matters worse, if I were on a real job and had people watching me it would seem like I am just slacking off. To make matters worst, there is always that temptation. You can always think of 100 reasons not to do work. Actually making up your mind to spend your precious life essence on things that seem like meaningless symbol pushing is tough.
    
    12:10pm. I'll have to go through this same process when it is time to do the partial evaluator.
    
    Yeah, I have great benefits due to past experience, but the times like now reveal just how much self aggrandizement that is. I need to put in real effort to do this.
    
    12:15pm. Let stop here."

--- The Spiral Language v0.2 (typechecking)/Prepass.fs
+++ The Spiral Language v0.2 (typechecking)/Prepass.fs
@@ -1,7 +1,8 @@
 ﻿module Spiral.Prepass
 
 type Id = int32
-type FreeVars = {|type' : int; term : int|}
+//type FreeVars = {|ty : int; term : int|}
+type FreeVars = unit
 type Range = { uri : string; range : Config.VSCRange }
 
 type Macro =
@@ -27,6 +28,7 @@ and Expr =
     | SymbolCreate of Range * string
     | Type of Range * TExpr
     | Fun of Range * FreeVars * Id * Expr
+    | Recursive of (Range * FreeVars * Id * Expr) ref
     | Forall of Range * FreeVars * Id * Expr
     | RecBlock of Range * ((Range * Id) * Expr) list * on_succ: Expr
     | RecordWith of Range * Expr list * RecordWith list * RecordWithout list
@@ -35,9 +37,8 @@ and Expr =
     | Annot of Range * Expr * TExpr
     | Typecase of Range * TExpr * (TExpr * Expr) list
     | ModuleOpen of Range * (Range * Id) * (Range * string) list * on_succ: Expr
-    | Apply of Range * Expr * Expr
+    
     | Macro of Range * Macro list
-    | Recursive of Range * Expr ref
     | Inline of Range * FreeVars * Expr
     // Regular pattern matching
     | Let of Range * Id * Expr * Expr
@@ -69,54 +70,12 @@ and TExpr =
     | Term of Range * Expr
     | Macro of Range * Macro list
     | Fun of Range * FreeVars * Id * TExpr
+    | HigherOrder of Range * Id
 
-//let pattern_to_rawexpr (arg: VarString, clauses: (Pattern * RawExpr) []) = 
-//    let mutable tag = 0
-//    let patvar () = 
-//        let x = sprintf " pat_var%i" tag
-//        tag <- tag + 1
-//        x
+//type TopEnv = {
+    
+//    } 
 
-//    let rec cp' is_dyned (arg: VarString) (pat: Pattern) (on_succ: RawExpr) (on_fail: RawExpr): RawExpr =
-//        let cp = cp' is_dyned
-//        let step pat on_succ = 
-//            match pat with
-//            | PatVar arg -> arg, on_succ
-//            | _ -> let arg = patvar() in arg, cp arg pat on_succ on_fail
-//        match pat with
-//        | PatOperator _ -> failwith "Compiler error: PatOperator case is an error that should be caught during validation."
-//        | PatB -> RawBTest(arg,on_succ,on_fail)
-//        | PatE -> on_succ
-//        | PatVar x -> l x (v arg) on_succ
-//        | PatDyn x when is_dyned -> cp arg x on_succ on_fail
-//        | PatDyn x -> let arg' = patvar() in l arg' (dyn (v arg)) (cp' true arg' x on_succ on_fail)
-//        | PatAnnot (exp,typ) ->
-//            let on_succ = cp arg exp on_succ on_fail
-//            RawAnnotTest(typ,arg,on_succ,on_fail)
-//        | PatPair(a,b) -> 
-//            let b,on_succ = step b on_succ
-//            let a,on_succ = step a on_succ
-//            RawPairTest(a,b,arg,on_succ,on_fail)
-//        | PatNominal(a,b) -> 
-//            let arg', on_succ = step b on_succ
-//            RawNominalTest(a,arg',arg,on_succ,on_fail)
-//        | PatSymbol x -> RawSymbolTest(x,arg,on_succ,on_fail)
-//        | PatActive (a,b) -> let arg', on_succ = step b on_succ in l arg' (ap a (v arg)) on_succ
-//        | PatOr(a,b) -> let on_succ = inline_ on_succ in cp arg a on_succ (cp arg b on_succ on_fail)
-//        | PatAnd(a,b) -> let on_fail = inline_ on_fail in cp arg a (cp arg b on_succ on_fail) on_fail
-//        | PatValue x -> RawValueTest(x,arg,on_succ,on_fail)
-//        | PatDefaultValue x -> RawDefaultLitTest(x,arg,on_succ,on_fail)
-//        | PatWhen(p, e) -> cp arg p (if' e on_succ on_fail) on_fail
-//        | PatUnbox p -> let arg', on_succ = step p on_succ in RawCase(arg', arg, on_succ)
-//        | PatRecordMembers items ->
-//            let binds, on_succ =
-//                List.mapFoldBack (fun item on_succ ->
-//                    match item with
-//                    | PatRecordMembersSymbol(keyword,name) -> let arg, on_succ = step name on_succ in RawRecordTestSymbol(keyword,arg), on_succ
-//                    | PatRecordMembersInjectVar(var,name) -> let arg, on_succ = step name on_succ in RawRecordTestInjectVar(var,arg), on_succ
-//                    ) items on_succ
-//            RawRecordTest(List.toArray binds,arg,on_succ,on_fail)
+let fill_and_rename top_env (expr : BlockParsing.RawExpr) =
+    ()
 
-//    Array.foldBack (fun (pat, exp) (i, on_fail) -> 
-//        i - 1, cp' false arg pat (if i <> 0 then inline_ exp else exp) on_fail
-//        ) clauses (clauses.Length - 1, RawOp(ErrorPatMiss, [|v arg|])) |> snd
