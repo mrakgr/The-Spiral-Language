2122c56c953ae6ec3dacadcf167fb885803177c3
Marko Grdinić <mrakgr@gmail.com>
2021-04-05 21:12:02 +0200

    "8:50pm.
    ```
    // Folds over the range in an downwards direction.
    inl forDown (nearFrom:to:) body state = for (from: to nearTo: nearFrom) (fun i => body (nearFrom-1-i)) state
    ```
    
    I started thinking about ranges, and actually isn't this wrong?
    
    ```
    let for' from nearTo body state =
        let rec loop i s = if i < nearTo then loop (i+1) (body i s) else s
        loop from state
    
    // Folds over the range in an downwards direction.
    let forDown nearFrom to' body state = for' to' nearFrom (fun i -> body (nearFrom-1-i)) state
    
    forDown 8 2 (fun i () -> printfn "%i" i) ()
    ```
    ```
    >
    5
    4
    3
    2
    1
    0
    ```
    
    Ah, fuck. It is wrong!
    
    ```
    // Folds over the range in an downwards direction.
    let forDown nearFrom to' body state = for' to' nearFrom (fun i -> body (nearFrom+to'-1-i)) state
    
    forDown 8 2 (fun i () -> printfn "%i" i) ()
    ```
    
    This is the right way to do it.
    
    To think I had this kind of error.
    
    ```
    let for' from nearTo body state =
        let rec loop i s = if i > nearTo then loop (i-1) (body i s) else s
        loop from state
    
    // Folds over the range in an downwards direction.
    let forUp nearFrom to' body state = for' to' nearFrom (fun i -> body (nearFrom-1-i)) state
    
    forUp 2 5 (fun i () -> printfn "%i" i) ()
    ```
    
    How would the opposite work? Not like this...
    
    ```
    inl forDown (nearFrom:to:) body state = for (from: to nearTo: nearFrom) (fun i => body ((to-i)+nearFrom-1)) state
    ```
    
    Let me implement it like so."

--- The Spiral Language 2/Script1.fsx
+++ The Spiral Language 2/Script1.fsx
@@ -1,1 +1,8 @@
-﻿
+﻿let for' from nearTo body state =
+    let rec loop i s = if i > nearTo then loop (i-1) (body i s) else s
+    loop from state
+
+// Folds over the range in an downwards direction.
+let forUp nearFrom to' body state = for' to' nearFrom (fun i -> body ((to'-i)+nearFrom+1)) state
+
+forUp 2 5 (fun i () -> printfn "%i" i) ()
