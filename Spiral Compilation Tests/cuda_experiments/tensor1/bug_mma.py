e93d923bd46548cede25ee01ac1bc2c01aa8bfa5
mrakgr <mrakgr@gmail.com>
2024-01-30 15:02:29 +0100

    It works when I try to compile it with NVRTC.

--- Spiral Compilation Tests/cuda_experiments/tensor1/bug_mma.py
+++ Spiral Compilation Tests/cuda_experiments/tensor1/bug_mma.py
@@ -3,7 +3,8 @@ import cupy as cp
 kernel_code = '''
 #include <mma.h>
 extern "C" __global__ void mykernel() {
+    printf("Hello.\\n");
 }
 '''
-module = cp.RawModule(code=kernel_code, backend='nvcc')
-kernel = module.get_function('mykernel')
+module = cp.RawModule(code=kernel_code, backend='nvrtc')
+module.get_function('mykernel')((1,),(1,),())
