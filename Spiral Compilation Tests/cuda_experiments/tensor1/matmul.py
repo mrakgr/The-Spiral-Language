631735725bfcaf3e1de7d294cbb48fb8696aca78
mrakgr <mrakgr@gmail.com>
2024-01-31 17:50:22 +0100

    Finally got the example to run and tested it out. Once I got it to run the first time it was all correct, so the actual testing part didn't take any time.
    Next are transposes. I'll leave those for tomorrow.
    
    Updated the Cuda device codegen for max NVRTC compatibility. v2.7.1 is out.

--- Spiral Compilation Tests/cuda_experiments/tensor1/matmul.py
+++ Spiral Compilation Tests/cuda_experiments/tensor1/matmul.py
@@ -6,10 +6,15 @@ using namespace nvcuda;
 using namespace cooperative_groups;
 __device__ inline bool while_method_0(long v0){
     bool v1;
-    v1 = v0 < 1l;
+    v1 = v0 < 256l;
     return v1;
 }
-__device__ inline bool while_method_1(long v0, long v1){
+__device__ inline bool while_method_1(long v0){
+    bool v1;
+    v1 = v0 < 16l;
+    return v1;
+}
+__device__ inline bool while_method_2(long v0, long v1){
     bool v2;
     v2 = v1 < v0;
     return v2;
@@ -23,90 +28,102 @@ extern "C" __global__ void entry0(float * v0, float * v1, float * v2) {
     v5 = v4;
     while (while_method_0(v5)){
         long v7;
-        v7 = v5 % 1l;
-        bool v8;
-        v8 = v5 == 0l;
-        bool v9;
-        v9 = v8 == false;
-        if (v9){
-            const char * v10;
-            v10 = "The index has to be in the range of the dimension.";
-            assert(v8 /* v10 */);
+        v7 = v5 % 16l;
+        long v8;
+        v8 = v5 / 16l;
+        long v9;
+        v9 = v8 % 16l;
+        long v10;
+        v10 = v8 / 16l;
+        bool v11;
+        v11 = v10 == 0l;
+        bool v12;
+        v12 = v11 == false;
+        if (v12){
+            const char * v13;
+            v13 = "The index has to be in the range of the dimension.";
+            assert(v11 /* v13 */);
         } else {
         }
-        assert(0 <= v7 && v7 < 1l /* Tensor range check */);
-        long v12;
-        v12 = 128l * v7;
-        assert(0 <= v7 && v7 < 1l /* Tensor range check */);
-        assert(0 <= v7 && v7 < 1l /* Tensor range check */);
-        assert(0 <= v7 && v7 < 1l /* Tensor range check */);
-        long v13;
-        v13 = 16l * v7;
-        long v14;
-        v14 = 256l * v7;
+        assert(0 <= v9 && v9 < 16l /* Tensor range check */);
         long v15;
-        v15 = v14 + v13;
-        wmma::fragment<wmma::accumulator, 16l, 16l, 8l, float> v16;
-        wmma::fill_fragment(v16, 0.0f);
+        v15 = 2048l * v9;
+        assert(0 <= v7 && v7 < 16l /* Tensor range check */);
+        long v16;
+        v16 = 2048l * v7;
+        assert(0 <= v9 && v9 < 16l /* Tensor range check */);
+        assert(0 <= v7 && v7 < 16l /* Tensor range check */);
         long v17;
-        v17 = 0l;
-        while (while_method_0(v17)){
-            long v19;
-            v19 = v17 % 1l;
-            bool v20;
-            v20 = v17 == 0l;
-            bool v21;
-            v21 = v20 == false;
-            if (v21){
-                const char * v22;
-                v22 = "The index has to be in the range of the dimension.";
-                assert(v20 /* v22 */);
+        v17 = 16l * v7;
+        long v18;
+        v18 = 4096l * v9;
+        long v19;
+        v19 = v18 + v17;
+        wmma::fragment<wmma::accumulator, 16l, 16l, 8l, float> v20;
+        wmma::fill_fragment(v20, 0.0f);
+        long v21;
+        v21 = 0l;
+        while (while_method_1(v21)){
+            long v23;
+            v23 = v21 % 16l;
+            long v24;
+            v24 = v21 / 16l;
+            bool v25;
+            v25 = v24 == 0l;
+            bool v26;
+            v26 = v25 == false;
+            if (v26){
+                const char * v27;
+                v27 = "The index has to be in the range of the dimension.";
+                assert(v25 /* v27 */);
             } else {
             }
-            assert(0 <= v19 && v19 < 1l /* Tensor range check */);
-            long v24;
-            v24 = 8l * v19;
-            long v25;
-            v25 = v24 + v12;
-            assert(0 <= v19 && v19 < 1l /* Tensor range check */);
-            wmma::fragment<wmma::matrix_a, 16l, 16l, 8l, wmma::precision::tf32, wmma::row_major> v26;
-            wmma::fragment<wmma::matrix_b, 16l, 16l, 8l, wmma::precision::tf32, wmma::col_major> v27;
-            float * v28;
-            v28 = v0 + v25;
-            wmma::load_matrix_sync(v26, v28, 8l);
+            assert(0 <= v23 && v23 < 16l /* Tensor range check */);
+            long v29;
+            v29 = 8l * v23;
+            long v30;
+            v30 = v29 + v15;
+            assert(0 <= v23 && v23 < 16l /* Tensor range check */);
+            long v31;
+            v31 = v29 + v16;
+            wmma::fragment<wmma::matrix_a, 16l, 16l, 8l, wmma::precision::tf32, wmma::row_major> v32;
+            wmma::fragment<wmma::matrix_b, 16l, 16l, 8l, wmma::precision::tf32, wmma::col_major> v33;
+            float * v34;
+            v34 = v0 + v30;
+            wmma::load_matrix_sync(v32, v34, 128l);
             #pragma unroll
-            for (int t = 0; t < v26.num_elements; t++) { v26.x[t] = wmma::__float_to_tf32(v26.x[t]); };
-            float * v29;
-            v29 = v1 + v25;
-            wmma::load_matrix_sync(v27, v29, 8l);
+            for (int t = 0; t < v32.num_elements; t++) { v32.x[t] = wmma::__float_to_tf32(v32.x[t]); };
+            float * v35;
+            v35 = v1 + v31;
+            wmma::load_matrix_sync(v33, v35, 128l);
             #pragma unroll
-            for (int t = 0; t < v27.num_elements; t++) { v27.x[t] = wmma::__float_to_tf32(v27.x[t]); };
-            wmma::mma_sync(v16, v26, v27, v16);
-            v17 += 1l ;
+            for (int t = 0; t < v33.num_elements; t++) { v33.x[t] = wmma::__float_to_tf32(v33.x[t]); };
+            wmma::mma_sync(v20, v32, v33, v20);
+            v21 += 1l ;
         }
-        wmma::fragment<wmma::accumulator, 16l, 16l, 8l, float> v30;
-        float * v31;
-        v31 = v2 + v15;
-        wmma::load_matrix_sync(v30, v31, 16l, wmma::mem_row_major);
-        long v32;
-        v32 = v30.num_elements;
-        long v33;
-        v33 = 0l;
-        while (while_method_1(v32, v33)){
-            float v35;
-            v35 = v16.x[v33];
-            float v36;
-            v36 = v30.x[v33];
-            float v37;
-            v37 = 0.0f * v36;
-            float v38;
-            v38 = v35 + v37;
-            v30.x[v33] = v38;
-            v33 += 1l ;
+        wmma::fragment<wmma::accumulator, 16l, 16l, 8l, float> v36;
+        float * v37;
+        v37 = v2 + v19;
+        wmma::load_matrix_sync(v36, v37, 256l, wmma::mem_row_major);
+        long v38;
+        v38 = v36.num_elements;
+        long v39;
+        v39 = 0l;
+        while (while_method_2(v38, v39)){
+            float v41;
+            v41 = v20.x[v39];
+            float v42;
+            v42 = v36.x[v39];
+            float v43;
+            v43 = 0.0f * v42;
+            float v44;
+            v44 = v41 + v43;
+            v36.x[v39] = v44;
+            v39 += 1l ;
         }
-        float * v39;
-        v39 = v2 + v15;
-        wmma::store_matrix_sync(v39, v30, 16l, wmma::mem_row_major);
+        float * v45;
+        v45 = v2 + v19;
+        wmma::store_matrix_sync(v45, v36, 256l, wmma::mem_row_major);
         v5 += v3 ;
     }
     return ;
@@ -121,9 +138,9 @@ options = []
 options.append('--diag-suppress=550')
 raw_module = cp.RawModule(code=kernel, backend='nvrtc', enable_cooperative_groups=True, options=tuple(options))
 def main():
-    v0 = cp.random.normal(0,1,128,cp.float32)
+    v0 = cp.random.normal(0,1,32768,cp.float32)
     v1 = v0.size
-    v2 = 128 == v1
+    v2 = 32768 == v1
     del v1
     v3 = v2 == False
     if v3:
@@ -133,9 +150,9 @@ def main():
     else:
         pass
     del v2, v3
-    v6 = cp.random.normal(0,1,128,cp.float32)
+    v6 = cp.random.normal(0,1,32768,cp.float32)
     v7 = v6.size
-    v8 = 128 == v7
+    v8 = 32768 == v7
     del v7
     v9 = v8 == False
     if v9:
@@ -145,9 +162,9 @@ def main():
     else:
         pass
     del v8, v9
-    v12 = cp.matmul(v6.reshape((16, 8)),cp.transpose(v0.reshape((16, 8)))).flatten()
+    v12 = cp.matmul(v6.reshape((256, 128)),cp.transpose(v0.reshape((256, 128)))).flatten()
     v13 = v12.size
-    v14 = 256 == v13
+    v14 = 65536 == v13
     del v13
     v15 = v14 == False
     if v15:
@@ -157,12 +174,10 @@ def main():
     else:
         pass
     del v14, v15
-    v18 = cp.empty(256,dtype=cp.float32)
+    v18 = cp.empty(65536,dtype=cp.float32)
     v19 = 0
     raw_module.get_function(f"entry{v19}")((24, 1, 1),(256, 1, 1),(v6, v0, v18))
     del v0, v6, v19
-    print(v18)
-    print(v12)
     v20 = cp.max(cp.abs(v18-v12))
     del v12, v18
     return v20
