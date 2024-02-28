77371cf962e82746c5770ecba7000d7c6a628e4f
mrakgr <mrakgr@gmail.com>
2024-02-28 10:23:13 +0100

    Starting work on the tensor printing function.

---
+++ Spiral Compilation Tests/cuda_experiments/tensor9/wmma_test.py
@@ -0,0 +1,48 @@
+kernel = r"""
+template <typename el, int dim> struct array { el v[dim]; };
+"""
+import cupy as cp
+from dataclasses import dataclass
+from typing import NamedTuple, Union, Callable, Tuple
+i8 = i16 = i32 = i64 = u8 = u16 = u32 = u64 = int; f32 = f64 = float; char = string = str
+
+def main():
+    v0 = cp.arange(0,64,1,dtype=cp.float64)
+    v1 = v0.size
+    v2 = 64 == v1
+    del v1
+    v3 = v2 == False
+    if v3:
+        v4 = "The total length of the reshaped tensor dimension must match that of the original one."
+        assert v2, v4
+        del v4
+    else:
+        pass
+    del v2, v3
+    v5 = cp.arange(0,32,1,dtype=cp.float64)
+    v6 = v5.size
+    v7 = 32 == v6
+    del v6
+    v8 = v7 == False
+    if v8:
+        v9 = "The total length of the reshaped tensor dimension must match that of the original one."
+        assert v7, v9
+        del v9
+    else:
+        pass
+    del v7, v8
+    v10 = cp.arange(0,32,1,dtype=cp.float64)
+    v11 = v10.size
+    v12 = 32 == v11
+    del v11
+    v13 = v12 == False
+    if v13:
+        v14 = "The total length of the reshaped tensor dimension must match that of the original one."
+        assert v12, v14
+        del v14
+    else:
+        pass
+    del v12, v13
+    return v10, v5, v0
+
+if __name__ == '__main__': print(main())
