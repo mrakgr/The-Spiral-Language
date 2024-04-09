f0986e9f77c8a301aa8ea81dcbd4ab21c5437204
mrakgr <mrakgr@gmail.com>
2024-04-09 20:30:30 +0200

    Did a lot of research for which learning algorithm I am going to use to optimize the poker agents. I'll get back to doing the video tomorrow. In the meantime, here is program to get the standard deviation for a given percentile of a Gaussian distribution.
    https://www.reddit.com/r/reinforcementlearning/comments/1byunu0/what_evolutionary_algorithm_should_i_use/

--- The Spiral Language 2/Script1.fsx
+++ The Spiral Language 2/Script1.fsx
@@ -1,4 +1,25 @@
 ﻿open System
 
-let x = Random()
-x.Next(0,52)
+// Gets the standard deviation of a Gaussian distribution at a given top percentile.
+let get_std_of_percentile percentile =
+
+    // Parameters for the normal distribution
+    let mean = 0.0
+    let standardDeviation = 1.0
+
+    // Number of samples
+    let sampleSize = 1 <<< 22
+
+    // Initialize the random number generator
+    let random = Random()
+
+    // Generate the sample
+    let normalSample =
+        Array.init sampleSize (fun _ ->
+            let z = random.NextDouble() // Generate a random number between 0 and 1
+            mean + standardDeviation * Math.Sqrt(-2.0 * Math.Log(z)) * Math.Cos(2.0 * Math.PI * random.NextDouble())
+        )
+
+    (Array.sort normalSample)[int((1.0 - percentile) * float normalSample.Length)]
+
+get_std_of_percentile (1.0 / 8.0)
