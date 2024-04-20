d412f08378584cfc34c4ce4fcae137ec6e4e0ad7
mrakgr <mrakgr@gmail.com>
2024-04-20 10:19:57 +0200

    We'll go with a web UI after all.

---
+++ Spiral Compilation Tests/cuda_experiments/ml2/rps_ui/ui/hello.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
