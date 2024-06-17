2d7fe874bfe0a58bd87891ff61a851fba6d74616
mrakgr <mrakgr@gmail.com>
2024-06-17 14:44:37 +0200

    Added a newline to the Python lib.

--- VS Code Plugin/publish.ps1
+++ VS Code Plugin/publish.ps1
@@ -6,8 +6,8 @@ $files = @(
 foreach ($file in $files) { # Copies the readme and the image files into the current folder so they get packed with the extension.
     Copy-Item ../$file .
 }
-vsce publish minor
-# vsce publish patch # https://marketplace.visualstudio.com/items?itemName=mrakgr.spiral-lang-vscode
+# vsce publish minor
+vsce publish patch # https://marketplace.visualstudio.com/items?itemName=mrakgr.spiral-lang-vscode
 npx ovsx publish -p $keys.ovsx # https://open-vsx.org/extension/mrakgr/spiral-lang-vscode
 foreach ($file in $files) { # Copies the readme and the image files in the current folder
     Remove-Item ./$file
