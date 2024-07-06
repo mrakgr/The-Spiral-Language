c6a348c10f9398e21ac3caa3ad175b8726df1a3b
mrakgr <mrakgr@gmail.com>
2024-07-06 17:46:12 +0200

    Bad news - all the vids I've been recording for the past week have no audio, so I'll have to turn them into a timelapse and slap the Umineko OST on top.
    Good news - I'll do a much shorter video explaining how to implement GADTs on top of Spiral. Call it a review.

--- Spiral Compilation Tests/compilation_tests/tutorial1/main.fsx
+++ Spiral Compilation Tests/compilation_tests/tutorial1/main.fsx
@@ -1,17 +1,19 @@
 type [<Struct>] US0 =
     | US0_0 of f0_0 : int32
 and [<Struct>] US1 =
-    | US1_0
-let rec method0 (v0 : US0) : unit =
+    | US1_0 of f0_0 : float32
+let rec method0 (v0 : US0) : int32 =
     match v0 with
     | US0_0(v1) -> (* A *)
-        ()
-and method1 (v0 : US1) : unit =
+        v1
+and method1 (v0 : US1) : float32 =
     match v0 with
-    | US1_0 -> (* B *)
-        ()
+    | US1_0(v1) -> (* B *)
+        v1
 let v0 : int32 = 2
 let v1 : US0 = US0_0(v0)
-method0(v1)
-let v2 : US1 = US1_0
-method1(v2)
+let v2 : int32 = method0(v1)
+let v3 : float32 = 3.0f
+let v4 : US1 = US1_0(v3)
+let v5 : float32 = method1(v4)
+()
