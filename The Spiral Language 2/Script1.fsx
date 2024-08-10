9749ac8b9ae308c9aaaa6ed886777e0357103697
mrakgr <mrakgr@gmail.com>
2024-08-10 19:24:09 +0200

    Cannot serve as a substitute for the discrete sampling function.

--- The Spiral Language 2/Script1.fsx
+++ The Spiral Language 2/Script1.fsx
@@ -1,5 +1,21 @@
-﻿let rec loop (x : int) : int =
-    printfn "%i" x
-    1 + loop (x+1)
-
-loop 0
+﻿open System
+let rng = Random()
+let normalize x =
+    let sum = x |> Array.sum
+    x |> Array.map (fun x -> x / sum)
+let probs = [|0.4;0.6|] |> normalize
+let f() =
+    probs
+    |> Array.mapi (fun i x -> 
+        let r = rng.NextDouble()
+        (if r < x then x else 0),
+        i
+        )
+    |> Array.max
+let counts = probs |> Array.map (fun _ -> 0)
+for i = 0 to 1 <<< 20 do
+    let x,i = f()
+    if x > 0 then
+        counts[i] <- counts[i] + 1
+let probs' =
+    counts |> Array.map float |> normalize
