ca340e97e9d23a71b10a927a267d3741e6aac160
mrakgr <mrakgr@gmail.com>
2024-12-01 11:42:51 +0100

    Accidentally bumped the version twice on VSCE.

--- VS Code Plugin/publish.ps1
+++ VS Code Plugin/publish.ps1
@@ -1,14 +1,36 @@
-$keys = Get-Content -Raw -Path .\keys.json | ConvertFrom-Json
-$files = @(
-    "readme.md"
-    "spiral_logo.png"
-)
-foreach ($file in $files) { # Copies the readme and the image files into the current folder so they get packed with the extension.
-    Copy-Item ../$file .
+# Suppresses warnings.
+$WarningPreference = 'SilentlyContinue'
+# This instructs PowerShell to treat non-terminating errors as terminating errors, which will halt script execution.
+$ErrorActionPreference = "Stop"
+
+function Publish-Vsce {
+    param (
+        [string] $Rank = "patch" # Can also be "minor"
+    )
+    Write-Host "Publishing on VSCE."
+    vsce publish $Rank # https://marketplace.visualstudio.com/items?itemName=mrakgr.spiral-lang-vscode
 }
-# vsce publish minor
-vsce publish patch # https://marketplace.visualstudio.com/items?itemName=mrakgr.spiral-lang-vscode
-npx ovsx publish -p $keys.ovsx # https://open-vsx.org/extension/mrakgr/spiral-lang-vscode
-foreach ($file in $files) { # Copies the readme and the image files in the current folder
-    Remove-Item ./$file
+
+function Publish-Ovsx {
+    Write-Host "Publishing on OVSX."
+    npx ovsx publish -p $keys.ovsx # https://open-vsx.org/extension/mrakgr/spiral-lang-vscode
+}
+
+try {
+    $keys = Get-Content -Raw -Path .\keys.json | ConvertFrom-Json
+    $files = @(
+        "readme.md"
+        "spiral_logo.png"
+    )
+    foreach ($file in $files) { # Copies the readme and the image files into the current folder so they get packed with the extension.
+        Copy-Item ../$file .
+    }
+    
+    Publish-Vsce -Rank "patch"
+    Publish-Ovsx
+}
+finally {
+    foreach ($file in $files) { # Copies the readme and the image files in the current folder
+        Remove-Item ./$file
+    }
 }
