9da96a44a3dfdd96643900f10dc4f45570a1aa36
Marko Grdinić <mrakgr@gmail.com>
2025-09-26 17:54:50 +0200

    Improved the publish script a little.

--- VS Code Plugin/publish.ps1
+++ VS Code Plugin/publish.ps1
@@ -25,13 +25,16 @@ function Publish-Ovsx {
     npx ovsx publish -p $keys.ovsx $prerelease_arg # https://open-vsx.org/extension/mrakgr/spiral-lang-vscode
 }
 
-foreach ($file in $files) { # Copies the readme and the image files into the current folder so they get packed with the extension.
-    Copy-Item ../$file .
-}
-
-Publish-Vsce
-Publish-Ovsx
-
-foreach ($file in $files) { # Removes the copied files.
-    Remove-Item ./$file
+try {
+    foreach ($file in $files) { # Copies the readme and the image files into the current folder so they get packed with the extension.
+        Copy-Item ../$file .
+    }
+    
+    Publish-Vsce
+    Publish-Ovsx
+    
+} finally {
+    foreach ($file in $files) { # Removes the copied files.
+        Remove-Item ./$file
+    }
 }
